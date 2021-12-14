6da97545ff96c020d0c1ce9c8de48c5fcedead67
SylvainDe <sylvain.desodt+github@gmail.com>
2021-12-14 20:31:50 +0100

    Year 2015 - Day 5 - part 1

--- 2015/day5.py
+++ 2015/day5.py
@@ -1,18 +1,41 @@
 # vi: set shiftwidth=4 tabstop=4 expandtab:
 import datetime
+import collections
 
 
-def get_xxx_from_file(file_path="day5_input.txt"):
+def get_strings_from_file(file_path="day5_input.txt"):
     with open(file_path) as f:
         return [l.strip() for l in f]
 
 
+def get_nb_vowels(s):
+    c = collections.Counter(s)
+    return sum(c[v] for v in "aeiou")
+
+
+def has_double_letter(s):
+    return any(c1 == c2 for c1, c2 in zip(s, s[1:]))
+
+
+def is_nice(s):
+    return (
+        get_nb_vowels(s) >= 3
+        and has_double_letter(s)
+        and not any(sub in s for sub in ("ab", "cd", "pq", "xy"))
+    )
+
+
 def run_tests():
-    xxx = some_hardcoded_value
+    assert is_nice("ugknbfddgicrmopn")
+    assert is_nice("aaa")
+    assert not is_nice("jchzalrnumimnmhp")
+    assert not is_nice("haegwjzuvuyypxyu")
+    assert not is_nice("dvszwmarrgswjxmb")
 
 
 def get_solutions():
-    xxx = get_xxx_from_file()
+    strings = get_strings_from_file()
+    print(sum(is_nice(s) for s in strings))
 
 
 if __name__ == "__main__":
