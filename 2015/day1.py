2c8eda8ec1fe83dd22c348dfa3e610ef26288c32
SylvainDe <sylvain.desodt+github@gmail.com>
2021-12-14 18:37:38 +0100

    Year 2015 - Day 1 - part 1

--- 2015/day1.py
+++ 2015/day1.py
@@ -1,18 +1,29 @@
 # vi: set shiftwidth=4 tabstop=4 expandtab:
 import datetime
+import collections
 
 
-def get_xxx_from_file(file_path="day1_input.txt"):
+def get_instructions_from_file(file_path="day1_input.txt"):
     with open(file_path) as f:
-        return [l.strip() for l in f]
+        for l in f:
+            return l.strip()
+
+
+def get_final_floor(instructions):
+    c = collections.Counter(instructions)
+    return c["("] - c[")"]
 
 
 def run_tests():
-    xxx = some_hardcoded_value
+    assert get_final_floor("(())") == 0
+    assert get_final_floor("()()") == 0
+    assert get_final_floor("(((") == 3
+    assert get_final_floor("())") == -1
 
 
 def get_solutions():
-    xxx = get_xxx_from_file()
+    instructions = get_instructions_from_file()
+    print(get_final_floor(instructions))
 
 
 if __name__ == "__main__":
