db909f3a0858aada6bd835c5b8ab3825a463ebef
SylvainDe <sylvain.desodt+github@gmail.com>
2021-12-14 20:15:50 +0100

    Year 2015 - Day 3 - part 1

--- 2015/day3.py
+++ 2015/day3.py
@@ -2,17 +2,43 @@
 import datetime
 
 
-def get_xxx_from_file(file_path="day3_input.txt"):
+def get_instructions_from_file(file_path="day3_input.txt"):
     with open(file_path) as f:
-        return [l.strip() for l in f]
+        for l in f:
+            return l.strip()
+
+
+directions = {
+    "<": (-1, 0),  # West
+    ">": (1, 0),  # East
+    "v": (0, -1),  # South
+    "^": (0, +1),  # North
+}
+
+
+def get_path(instructions):
+    x, y = 0, 0
+    yield x, y
+    for ins in instructions:
+        dx, dy = directions[ins]
+        x += dx
+        y += dy
+        yield x, y
+
+
+def get_nb_houses(instructions):
+    return len(set(get_path(instructions)))
 
 
 def run_tests():
-    xxx = some_hardcoded_value
+    assert get_nb_houses(">") == 2
+    assert get_nb_houses("^>v<") == 4
+    assert get_nb_houses("^v^v^v^v^v") == 2
 
 
 def get_solutions():
-    xxx = get_xxx_from_file()
+    instructions = get_instructions_from_file()
+    print(get_nb_houses(instructions))
 
 
 if __name__ == "__main__":
