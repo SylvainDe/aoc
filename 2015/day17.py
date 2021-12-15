ad18fcca6faf9a29cc05bb51766a400e8bef8a7e
SylvainDe <sylvain.desodt+github@gmail.com>
2021-12-15 22:13:13 +0100

    Year 2015 - Day 17 - Part 2

--- 2015/day17.py
+++ 2015/day17.py
@@ -18,13 +18,28 @@ def get_nb_ways(volume, containers):
     return ways[0]
 
 
+def get_nb_ways2(volume, containers):
+    ways = collections.Counter([(volume, 0)])
+    for c in sorted(containers):
+        for (rem_vol, nb_container), count in list(ways.items()):
+            vol2 = rem_vol - c
+            if vol2 >= 0:
+                ways[(vol2, nb_container + 1)] += count
+    min_cont = min(
+        nb_container for rem_vol, nb_container in ways.keys() if rem_vol == 0
+    )
+    return ways[(0, min_cont)]
+
+
 def run_tests():
     assert get_nb_ways(25, [20, 15, 10, 5, 5]) == 4
+    assert get_nb_ways2(25, [20, 15, 10, 5, 5]) == 3
 
 
 def get_solutions():
     containers = get_containers_from_file()
     print(get_nb_ways(150, containers))
+    print(get_nb_ways2(150, containers))
 
 
 if __name__ == "__main__":
