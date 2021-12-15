43e0d3245b505a256b6539ffd3080b021dbd1543
SylvainDe <sylvain.desodt+github@gmail.com>
2021-12-15 13:01:22 +0100

    Year 2015 - Day 8 - Part 1

--- 2015/day8.py
+++ 2015/day8.py
@@ -2,17 +2,38 @@
 import datetime
 
 
-def get_xxx_from_file(file_path="day8_input.txt"):
+def get_strings_from_file(file_path="day8_input.txt"):
     with open(file_path) as f:
         return [l.strip() for l in f]
 
 
+def get_str_len_diff(s):
+    char_diff = {
+        "\\": 1,
+        '"': 1,
+        "x": 3,
+    }
+    count = 2  # first and last quotes
+    skip = False
+    for c1, c2 in zip(s, s[1:]):
+        if skip:
+            skip = False
+        elif c1 == "\\":
+            count += char_diff[c2]
+            skip = True
+    return count
+
+
 def run_tests():
-    xxx = some_hardcoded_value
+    assert get_str_len_diff('""') == 2
+    assert get_str_len_diff('"abc"') == 2
+    assert get_str_len_diff('"aaa\\"aaa"') == 3
+    assert get_str_len_diff('"\\x27"') == 5
 
 
 def get_solutions():
-    xxx = get_xxx_from_file()
+    strings = get_strings_from_file()
+    print(sum(get_str_len_diff(s) for s in strings))
 
 
 if __name__ == "__main__":
