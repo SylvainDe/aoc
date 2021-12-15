64fd62bf5f686184dff950d1827a7eed5c0488c3
SylvainDe <sylvain.desodt+github@gmail.com>
2021-12-15 21:59:51 +0100

    Year 2015 - Day 17 - Part 1

--- 2015/day17.py
+++ 2015/day17.py
@@ -1,18 +1,30 @@
 # vi: set shiftwidth=4 tabstop=4 expandtab:
 import datetime
+import collections
 
 
-def get_xxx_from_file(file_path="day17_input.txt"):
+def get_containers_from_file(file_path="day17_input.txt"):
     with open(file_path) as f:
-        return [l.strip() for l in f]
+        return [int(l.strip()) for l in f]
+
+
+def get_nb_ways(volume, containers):
+    ways = collections.Counter([volume])
+    for c in sorted(containers):
+        for vol, count in list(ways.items()):
+            vol2 = vol - c
+            if vol2 >= 0:
+                ways[vol2] += count
+    return ways[0]
 
 
 def run_tests():
-    xxx = ""
+    assert get_nb_ways(25, [20, 15, 10, 5, 5]) == 4
 
 
 def get_solutions():
-    xxx = get_xxx_from_file()
+    containers = get_containers_from_file()
+    print(get_nb_ways(150, containers))
 
 
 if __name__ == "__main__":
