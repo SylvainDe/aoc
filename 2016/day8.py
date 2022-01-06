dcbc02387d709882f78d96c3ef4274603b85a4ab
SylvainDe <sylvain.desodt+github@gmail.com>
2022-01-06 22:24:09 +0100

    2016 - Day 8 - part 1

--- 2016/day8.py
+++ 2016/day8.py
@@ -2,17 +2,64 @@
 import datetime
 
 
-def get_xxx_from_file(file_path="day8_input.txt"):
+def get_instructions_from_file(file_path="day8_input.txt"):
     with open(file_path) as f:
         return [l.strip() for l in f]
 
 
+def show_pixels(pixels):
+    for y in range(height):
+        print("".join("#" if (x, y) in pixels else "." for x in range(width)))
+    print()
+
+
+def apply_instruction(instruction, pixels, width, height):
+    instr_lst = instruction.split(" ")
+    cmd = instr_lst[0]
+    param1 = instr_lst[1]
+    if cmd == "rect":
+        a, b = param1.split("x")
+        return {
+            (x % width, y % height) for x in range(int(a)) for y in range(int(b))
+        } | pixels
+    elif cmd == "rotate":
+        a = int(instr_lst[2][2:])
+        b = int(instr_lst[4])
+
+        if param1 == "column":
+            dx, dy = 0, b
+        elif param1 == "row":
+            dx, dy = b, 0
+        else:
+            assert False
+        return {
+            ((x + dx * (y == a)) % width, (y + dy * (x == a)) % height)
+            for (x, y) in pixels
+        }
+    else:
+        assert False
+
+
+def apply_instructions(instructions, width, height):
+    pixels = set()
+    for i in instructions:
+        pixels = apply_instruction(i, pixels, width, height)
+    return pixels
+
+
 def run_tests():
-    xxx = ""
+    instructions = [
+        "rect 3x2",
+        "rotate column x=1 by 1",
+        "rotate row y=0 by 4",
+        "rotate column x=1 by 1",
+    ]
+    assert len(apply_instructions(instructions, width=7, height=3))
 
 
 def get_solutions():
-    xxx = get_xxx_from_file()
+    instructions = get_instructions_from_file()
+    print(len(apply_instructions(instructions, width=50, height=6)))
 
 
 if __name__ == "__main__":
