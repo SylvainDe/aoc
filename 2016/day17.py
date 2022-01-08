9d5f8f79085780a668da927cea5ad5e582cee414
SylvainDe <sylvain.desodt+github@gmail.com>
2022-01-08 23:34:52 +0100

    2016 - Day 17 - part 1

--- 2016/day17.py
+++ 2016/day17.py
@@ -1,18 +1,70 @@
 # vi: set shiftwidth=4 tabstop=4 expandtab:
 import datetime
+import hashlib
+import collections
 
 
-def get_xxx_from_file(file_path="day17_input.txt"):
+def get_passcode_from_file(file_path="day17_input.txt"):
     with open(file_path) as f:
-        return [l.strip() for l in f]
+        for l in f:
+            return l.strip()
+
+
+def directions(path):
+    h = hashlib.md5(path.encode("utf-8")).hexdigest()
+    return [direction for char, direction in zip(h, "UDLR") if char in "bcdef"]
+
+
+dir_coord = {
+    "U": (0, -1),
+    "D": (0, 1),
+    "L": (-1, 0),
+    "R": (1, 0),
+}
+
+
+def point_in_dir(point, direction):
+    x, y = point
+    dx, dy = dir_coord[direction]
+    return (x + dx, y + dy)
+
+
+def point_is_valid(point):
+    x, y = point
+    return 0 <= x <= 3 and 0 <= y <= 3
+
+
+def shortest_path(passcode):
+    start = (0, 0)
+    end = (3, 3)
+    paths = collections.deque([(passcode, start)])
+    while paths:
+        path, pos = paths.popleft()
+        if pos == end:
+            assert path.startswith(passcode)
+            return path[len(passcode) :]
+        for d in directions(path):
+            pos2 = point_in_dir(pos, d)
+            if point_is_valid(pos2):
+                paths.append((path + d, pos2))
+    return None
 
 
 def run_tests():
-    xxx = ""
+    assert directions("hijkl") == ["U", "D", "L"]
+    assert directions("hijklD") == ["U", "L", "R"]
+    assert directions("hijklDR") == []
+    assert directions("hijklDU") == ["R"]
+    assert directions("hijklDUR") == []
+    assert shortest_path("hijkl") == None
+    assert shortest_path("ihgpwlah") == "DDRRRD"
+    assert shortest_path("kglvqrro") == "DDUDRLRRUDRD"
+    assert shortest_path("ulqzkmiv") == "DRURDRUDDLLDLUURRDULRLDUUDDDRR"
 
 
 def get_solutions():
-    xxx = get_xxx_from_file()
+    passcode = get_passcode_from_file()
+    print(shortest_path(passcode))
 
 
 if __name__ == "__main__":
