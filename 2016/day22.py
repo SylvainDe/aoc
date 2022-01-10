010a2956f88d4200318da7ae9c20f13589d3b7b6
SylvainDe <sylvain.desodt+github@gmail.com>
2022-01-10 18:32:30 +0100

    2016 - Day 22 - part 1

--- 2016/day22.py
+++ 2016/day22.py
@@ -1,18 +1,45 @@
 # vi: set shiftwidth=4 tabstop=4 expandtab:
 import datetime
+import collections
+import re
+import itertools
 
 
-def get_xxx_from_file(file_path="day22_input.txt"):
+# Filesystem              Size  Used  Avail  Use%
+Node = collections.namedtuple(
+    "Node", ("filesystem", "size", "used", "available", "percent_use")
+)
+
+# /dev/grid/node-x0-y0     85T   69T    16T   81%
+node_re = r"^(?P<fs>[^ ]+) +(?P<size>\d+)T +(?P<used>\d+)T +(?P<avail>\d+)T +(?P<percent>\d+)%$"
+
+
+def get_node_from_str(s):
+    match = re.match(node_re, s)
+    d = match.groupdict()
+    return Node(
+        d["fs"], int(d["size"]), int(d["used"]), int(d["avail"]), int(d["percent"])
+    )
+
+
+def get_nodes_from_file(file_path="day22_input.txt"):
     with open(file_path) as f:
-        return [l.strip() for l in f]
+        lines = [l.strip() for l in f]
+        return [get_node_from_str(s) for s in lines[2:]]
 
 
 def run_tests():
-    xxx = ""
+    nodes = ""
 
 
 def get_solutions():
-    xxx = get_xxx_from_file()
+    nodes = get_nodes_from_file()
+    pairs = [
+        (n1, n2)
+        for n1, n2 in itertools.product(nodes, nodes)
+        if n1.used != 0 and n1 != n2 and n1.used <= n2.available
+    ]
+    print(len(pairs))
 
 
 if __name__ == "__main__":
