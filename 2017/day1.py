cc41d7177194fc0fb932abe320219a600ed68cae
SylvainDe <sylvain.desodt+github@gmail.com>
2022-01-11 19:44:16 +0100

    2017 - Day 1 - part 1

--- 2017/day1.py
+++ 2017/day1.py
@@ -2,17 +2,29 @@
 import datetime
 
 
-def get_xxx_from_file(file_path="day1_input.txt"):
+def get_line_from_file(file_path="day1_input.txt"):
     with open(file_path) as f:
-        return [l.strip() for l in f]
+        for l in f:
+            return l.strip()
+
+
+def get_solution(line):
+    if not line:
+        return 0
+    return sum(int(c1) for c1, c2 in zip(line, line[1:] + line[0]) if c1 == c2)
 
 
 def run_tests():
-    xxx = ""
+    assert get_solution("") == 0
+    assert get_solution("1122") == 3
+    assert get_solution("1111") == 4
+    assert get_solution("1234") == 0
+    assert get_solution("91212129") == 9
 
 
 def get_solutions():
-    xxx = get_xxx_from_file()
+    line = get_line_from_file()
+    print(get_solution(line))
 
 
 if __name__ == "__main__":
