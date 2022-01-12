af3d25a6285660014945dce6670eaa6499a444f4
SylvainDe <sylvain.desodt+github@gmail.com>
2022-01-12 09:35:33 +0100

    2017 - Day 2 - part 2

--- 2017/day2.py
+++ 2017/day2.py
@@ -1,5 +1,6 @@
 # vi: set shiftwidth=4 tabstop=4 expandtab:
 import datetime
+import itertools
 
 
 def get_numbers_from_line(s):
@@ -15,6 +16,17 @@ def get_checksum(spreadsheet):
     return sum(max(l) - min(l) for l in spreadsheet)
 
 
+def find_div(numbers):
+    divs = [divmod(a, b) for a, b in itertools.permutations(numbers, 2)]
+    cand = [q for q, r in divs if r == 0]
+    assert len(cand) == 1
+    return cand[0]
+
+
+def get_div_sum(spreadsheet):
+    return sum(find_div(l) for l in spreadsheet)
+
+
 def run_tests():
     spreadsheet = [
         "5 1 9 5",
@@ -22,12 +34,20 @@ def run_tests():
         "2 4 6 8",
     ]
     spreadsheet = [get_numbers_from_line(l) for l in spreadsheet]
-    print(get_checksum(spreadsheet))
+    assert get_checksum(spreadsheet) == 18
+    spreadsheet = [
+        "5 9 2 8",
+        "9 4 7 3",
+        "3 8 6 5",
+    ]
+    spreadsheet = [get_numbers_from_line(l) for l in spreadsheet]
+    assert get_div_sum(spreadsheet) == 9
 
 
 def get_solutions():
     spreadsheet = get_spreadsheet_from_file()
     print(get_checksum(spreadsheet))
+    print(get_div_sum(spreadsheet))
 
 
 if __name__ == "__main__":
