2dc26c0f8e064cfb37d99deffd7ff39ae3a7a905
SylvainDe <sylvain.desodt+github@gmail.com>
2022-01-12 09:19:25 +0100

    2017 - Day 2 - part 1

--- 2017/day2.py
+++ 2017/day2.py
@@ -2,17 +2,32 @@
 import datetime
 
 
-def get_xxx_from_file(file_path="day2_input.txt"):
+def get_numbers_from_line(s):
+    return [int(v) for v in s.split()]
+
+
+def get_spreadsheet_from_file(file_path="day2_input.txt"):
     with open(file_path) as f:
-        return [l.strip() for l in f]
+        return [get_numbers_from_line(l.strip()) for l in f]
+
+
+def get_checksum(spreadsheet):
+    return sum(max(l) - min(l) for l in spreadsheet)
 
 
 def run_tests():
-    xxx = ""
+    spreadsheet = [
+        "5 1 9 5",
+        "7 5 3",
+        "2 4 6 8",
+    ]
+    spreadsheet = [get_numbers_from_line(l) for l in spreadsheet]
+    print(get_checksum(spreadsheet))
 
 
 def get_solutions():
-    xxx = get_xxx_from_file()
+    spreadsheet = get_spreadsheet_from_file()
+    print(get_checksum(spreadsheet))
 
 
 if __name__ == "__main__":
