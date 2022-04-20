92a4a9c4225c340db0a7c321e3e61a0c225f49e4
SylvainDe <sylvain.desodt+github@gmail.com>
2022-04-20 18:23:51 +0200

    2017 - Day 7 - part 2

--- 2017/day7.py
+++ 2017/day7.py
@@ -1,6 +1,7 @@
 # vi: set shiftwidth=4 tabstop=4 expandtab:
 import datetime
 import re
+import collections
 
 left_re = r"(.*) \((\d+)\)"
 
@@ -28,6 +29,35 @@ def get_bottom(programs):
     return bottoms[0]
 
 
+def get_wrong_disc(programs):
+    unweighted_programs = dict(programs)
+    weights = dict()
+    disk_weights = dict()
+    while unweighted_programs:
+        for name, (weight, lst) in unweighted_programs.items():
+            subweights_dict = {p: weights.get(p, None) for p in lst}
+            subweights = subweights_dict.values()
+            if not all(subweights):
+                continue
+            c = collections.Counter(subweights)
+            if len(c) > 1:
+                # If we have more than 1 weight, we expect a most common one to be correct and another one to exist only once and be wrong
+                (mc, nb_mc), (lc, nb_lc) = c.most_common()
+                assert nb_lc == 1
+                # Compute the desired weight based on the actual weight
+                for p, w in subweights_dict.items():
+                    if w == lc:
+                        return disk_weights[p] + mc - lc
+                else:
+                    assert False, "Did not find weight"
+            disk_weights[name] = weight
+            weights[name] = weight + sum(subweights)
+            unweighted_programs.pop(name)
+            break
+        else:  # no break
+            assert False, "Cannot compute remaining weights"
+
+
 def run_tests():
     prog = [
         "pbga (66)",
@@ -46,11 +76,13 @@ def run_tests():
     ]
     prog = dict(get_program_from_string(s) for s in prog)
     assert get_bottom(prog) == "tknk"
+    assert get_wrong_disc(prog) == 60
 
 
 def get_solutions():
     prog = get_prog_from_file()
     print(get_bottom(prog))
+    print(get_wrong_disc(prog))
 
 
 if __name__ == "__main__":
