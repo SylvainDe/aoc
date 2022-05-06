427b2a7fe9beb2306ed15e26c1712f38d4ab0ecc
SylvainDe <sylvain.desodt+github@gmail.com>
2022-05-06 08:53:36 +0200

    2021 - Day 1 - part 1

--- rust/day1/src/main.rs
+++ rust/day1/src/main.rs
@@ -1,3 +1,4 @@
+use itertools::Itertools;
 use std::fs::File;
 use std::io::BufRead;
 use std::io::BufReader;
@@ -17,17 +18,28 @@ fn get_input(filepath: &str) -> Vec<Int> {
         .collect()
 }
 
+fn part1(depths: Vec<Int>) -> usize {
+    depths.iter().tuple_windows().filter(|(a, b)| a < b).count()
+}
+
 fn main() {
     println!("Hello, world!");
     let numbers = get_input(INPUT_FILEPATH);
-    let s: Int = numbers.iter().sum();
-    println!("{:?}", s);
+    println!("{:?}", part1(numbers));
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn part1_provided_test() {
+        let numbers: Vec<Int> = vec![99, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+        assert_eq!(part1(numbers), 7);
     }
 }
