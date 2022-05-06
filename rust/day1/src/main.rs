d9113d7f03c2c62aab56d54a632a433797ccd004
SylvainDe <sylvain.desodt+github@gmail.com>
2022-05-06 08:27:01 +0200

    2021 - Day 1 - rewrite parse input

--- rust/day1/src/main.rs
+++ rust/day1/src/main.rs
@@ -4,20 +4,24 @@ use std::io::BufReader;
 
 const INPUT_FILEPATH: &str = "res/input.txt";
 
-fn get_input(filepath: &str) -> Option<Vec<u32>> {
-    let file = File::open(filepath).ok()?;
-    let mut numbers: Vec<u32> = vec![];
-    for line in BufReader::new(file).lines() {
-        let n: u32 = line.ok()?.parse().ok()?;
-        numbers.push(n)
-    }
-    Some(numbers)
+type Int = u32;
+
+fn get_input(filepath: &str) -> Vec<Int> {
+    BufReader::new(File::open(filepath).expect("Could not open file"))
+        .lines()
+        .map(|line| {
+            line.unwrap()
+                .parse::<Int>()
+                .expect("Could not convert line to integer")
+        })
+        .collect()
 }
 
 fn main() {
     println!("Hello, world!");
     let numbers = get_input(INPUT_FILEPATH);
-    println!("{:?}", numbers);
+    let s: Int = numbers.iter().sum();
+    println!("{:?}", s);
 }
 
 #[cfg(test)]
