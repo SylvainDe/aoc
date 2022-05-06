3ad2a1cff35b6537931326465f5caa8968578a4b
SylvainDe <sylvain.desodt+github@gmail.com>
2022-05-06 09:05:53 +0200

    2021 - Day 1 - part 2

--- rust/day1/src/main.rs
+++ rust/day1/src/main.rs
@@ -18,14 +18,26 @@ fn get_input(filepath: &str) -> Vec<Int> {
         .collect()
 }
 
-fn part1(depths: Vec<Int>) -> usize {
+fn part1(depths: &[Int]) -> usize {
     depths.iter().tuple_windows().filter(|(a, b)| a < b).count()
+}
+
+fn part2(depths: &[Int]) -> usize {
+    // Window(n + 1) > Window(n)
+    // d(n+1) + d(n+2) + ... + d(n+1+windowsize) > d(n) + d(n+1) + ... + d(n+windowsize)
+    // d(n+1+windowsize) > d(n)
+    depths
+        .iter()
+        .tuple_windows()
+        .filter(|(a, _, _, d)| a < d)
+        .count()
 }
 
 fn main() {
     println!("Hello, world!");
     let numbers = get_input(INPUT_FILEPATH);
-    println!("{:?}", part1(numbers));
+    println!("{:?}", part1(&numbers));
+    println!("{:?}", part2(&numbers));
 }
 
 #[cfg(test)]
@@ -33,13 +45,14 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn part1_provided_test() {
+        let numbers: Vec<Int> = vec![99, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+        assert_eq!(part1(&numbers), 7);
     }
 
     #[test]
-    fn part1_provided_test() {
+    fn part2_provided_test() {
         let numbers: Vec<Int> = vec![99, 200, 208, 210, 200, 207, 240, 269, 260, 263];
-        assert_eq!(part1(numbers), 7);
+        assert_eq!(part2(&numbers), 5);
     }
 }
