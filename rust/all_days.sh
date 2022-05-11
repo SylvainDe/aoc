9b489e860b544f598320c2e8fcfc54bf4cfde4e2
SylvainDe <sylvain.desodt+github@gmail.com>
2022-05-11 13:07:16 +0200

    Add script to run binaries for all days (and slightly more)

---
+++ rust/all_days.sh
@@ -0,0 +1,11 @@
+set -x
+set -e
+
+# Parse error "available binaries: bin_name1, bin_name2, bin_name3, bin_name4, bin_name5, bin_name6"
+for bin in $(cargo run 2>&1 >/dev/null | grep "available binaries" | sed "s/^available binaries://g" | sed "s/,//g");
+do
+    cargo run --bin "${bin}"
+done
+
+cargo test
+cargo clippy
