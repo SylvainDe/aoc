ef3c9185eb6f06a968b92f00ac83d3401961cc05
SylvainDe <sylvain.desodt+github@gmail.com>
2022-05-12 13:43:09 +0200

    2021 - Day 10 - part 1

--- rust/src/2021/day10/main.rs
+++ rust/src/2021/day10/main.rs
@@ -3,18 +3,59 @@ use std::fs;
 const INPUT_FILEPATH: &str = "res/2021/day10/input.txt";
 
 type Int = u32;
-type InputContent = Int;
+type InputContent = Vec<String>;
 
-fn get_input_from_str(_string: &str) -> InputContent {
-    0
+fn get_input_from_str(string: &str) -> InputContent {
+    string.lines().map(|l| l.to_string()).collect()
 }
 
 fn get_input_from_file(filepath: &str) -> InputContent {
     get_input_from_str(&fs::read_to_string(filepath).expect("Could not open file"))
 }
 
-fn part1(_arg: &InputContent) -> Int {
-    0
+#[derive(Debug, PartialEq)]
+enum ParseResult {
+    Success,
+    Corrupted(char),
+    // Incomplete,
+}
+
+fn parse_string(s: &str) -> ParseResult {
+    let mut stack: Vec<char> = Vec::new();
+    for c in s.chars() {
+        match c {
+            '(' => stack.push(')'),
+            '{' => stack.push('}'),
+            '[' => stack.push(']'),
+            '<' => stack.push('>'),
+            _ => match stack.pop() {
+                None => return ParseResult::Corrupted(c),
+                Some(expected) => {
+                    if expected != c {
+                        return ParseResult::Corrupted(c);
+                    }
+                }
+            },
+        }
+    }
+    ParseResult::Success
+}
+
+fn syntax_error_score(s: &str) -> Int {
+    match parse_string(s) {
+        ParseResult::Corrupted(c) => match c {
+            ')' => 3,
+            ']' => 57,
+            '}' => 1197,
+            '>' => 25137,
+            _ => panic!("Unexpected value {}", c),
+        },
+        _ => 0,
+    }
+}
+
+fn part1(strings: &InputContent) -> Int {
+    strings.iter().map(|s| syntax_error_score(s)).sum()
 }
 
 fn part2(_arg: &InputContent) -> Int {
@@ -22,11 +63,11 @@ fn part2(_arg: &InputContent) -> Int {
 }
 
 fn main() {
-    let data = get_input_from_file(INPUT_FILEPATH);
-    let res = part1(&data);
+    let strings = get_input_from_file(INPUT_FILEPATH);
+    let res = part1(&strings);
     println!("{:?}", res);
-    assert_eq!(res, 0);
-    let res2 = part2(&data);
+    assert_eq!(res, 339411);
+    let res2 = part2(&strings);
     println!("{:?}", res2);
     assert_eq!(res2, 0);
 }
@@ -35,11 +76,72 @@ fn main() {
 mod tests {
     use super::*;
 
-    const EXAMPLE: &str = "";
+    const EXAMPLE: &str = "[({(<(())[]>[[{[]{<()<>>
+[(()[<>])]({[<{<<[]>>(
+{([(<{}[<>[]}>{[]{[(<()>
+(((({<>}<{<{<>}{[]{[]{}
+[[<[([]))<([[{}[[()]]]
+[{[{({}]{}}([{[{{{}}([]
+{<[[]]>}<{[{[{[]{()[[[]
+[<(<(<(<{}))><([]([]()
+<{([([[(<>()){}]>(<<{{
+<{([{{}}[<[[[<>{}]]]>[]]";
+
+    #[test]
+    fn test_parse_legal_string() {
+        assert_eq!(parse_string("()"), ParseResult::Success);
+        assert_eq!(parse_string("[]"), ParseResult::Success);
+        assert_eq!(parse_string("([])"), ParseResult::Success);
+        assert_eq!(parse_string("{()()()}"), ParseResult::Success);
+        assert_eq!(parse_string("<([{}])>"), ParseResult::Success);
+        assert_eq!(parse_string("[<>({}){}[([])<>]]"), ParseResult::Success);
+        assert_eq!(parse_string("(((((((((())))))))))"), ParseResult::Success);
+    }
+
+    #[test]
+    fn test_parse_corrupted_string() {
+        assert_eq!(parse_string("(]"), ParseResult::Corrupted(']'));
+        assert_eq!(parse_string("{()()()>"), ParseResult::Corrupted('>'));
+        assert_eq!(parse_string("(((()))}"), ParseResult::Corrupted('}'));
+        assert_eq!(parse_string("<([]){()}[{}])"), ParseResult::Corrupted(')'));
+        //- Expected ], but found } instead.
+        assert_eq!(
+            parse_string("{([(<{}[<>[]}>{[]{[(<()>"),
+            ParseResult::Corrupted('}')
+        );
+        // Expected ], but found ) instead.
+        assert_eq!(
+            parse_string("[[<[([]))<([[{}[[()]]]"),
+            ParseResult::Corrupted(')')
+        );
+        // Expected ), but found ] instead.
+        assert_eq!(
+            parse_string("[{[{({}]{}}([{[{{{}}([]"),
+            ParseResult::Corrupted(']')
+        );
+        // Expected >, but found ) instead.
+        assert_eq!(
+            parse_string("[<(<(<(<{}))><([]([]()"),
+            ParseResult::Corrupted(')')
+        );
+        // Expected ], but found > instead
+        assert_eq!(
+            parse_string("<{([([[(<>()){}]>(<<{{"),
+            ParseResult::Corrupted('>')
+        );
+    }
+
+    #[test]
+    fn test_parse_strings_example() {
+        for s in get_input_from_str(EXAMPLE) {
+            // Nothing is checked yet
+            parse_string(&s);
+        }
+    }
 
     #[test]
     fn test_part1() {
-        assert_eq!(part1(&get_input_from_str(EXAMPLE)), 0);
+        assert_eq!(part1(&get_input_from_str(EXAMPLE)), 26397);
     }
 
     #[test]
