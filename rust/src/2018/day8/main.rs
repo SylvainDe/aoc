39b8c4074fc585e7165fdf1ce07b668258d49aa5
SylvainDe <sylvain.desodt+github@gmail.com>
2022-05-20 22:47:12 +0200

    Rust - 2018 - Day 8 - part 2

--- rust/src/2018/day8/main.rs
+++ rust/src/2018/day8/main.rs
@@ -3,7 +3,7 @@ use std::time::Instant;
 
 const INPUT_FILEPATH: &str = "../resources/year2018_day8_input.txt";
 
-type Int = u32;
+type Int = usize;
 type InputContent = Vec<Int>;
 
 fn get_input_from_str(string: &str) -> InputContent {
@@ -20,34 +20,43 @@ fn get_input_from_file(filepath: &str) -> InputContent {
     get_input_from_str(&fs::read_to_string(filepath).expect("Could not open file"))
 }
 
-fn extract_metadata(nbs: &[Int]) -> (Int, &[Int]) {
+fn compute_scores(nbs: &[Int], part1: bool) -> (Int, &[Int]) {
     //dbg!("start", nbs.len());
     let nb_child = nbs[0];
     let nb_meta = nbs[1];
     let mut remaining = &nbs[2..];
-    let mut ret = 0;
+    let mut childscores = Vec::<Int>::new();
+    // Parse children
     for _ in 0..nb_child {
         let meta;
-        (meta, remaining) = extract_metadata(remaining);
-        ret += meta;
+        (meta, remaining) = compute_scores(remaining, part1);
+        childscores.push(meta);
     }
-    for _ in 0..nb_meta {
-        ret += remaining[0];
-        remaining = &remaining[1..];
-    }
+    // Collect meta
+    let meta = &remaining[0..nb_meta];
+    remaining = &remaining[nb_meta..];
+    // Compute final value based on rules applied
+    let ret = if part1 || nb_child == 0 {
+        childscores.iter().sum::<Int>() + meta.iter().sum::<Int>()
+    } else {
+        meta.iter()
+            .map(|m| childscores.get(m - 1).unwrap_or(&0))
+            .sum()
+    };
     //dbg!("end", ret, remaining.len());
     (ret, remaining)
 }
 
 fn part1(nbs: &InputContent) -> Int {
-    let (nb, rem) = extract_metadata(nbs);
+    let (nb, rem) = compute_scores(nbs, true);
     assert!(rem.is_empty());
     nb
 }
 
-#[allow(clippy::trivially_copy_pass_by_ref, clippy::missing_const_for_fn)]
-fn part2(_arg: &InputContent) -> Int {
-    0
+fn part2(nbs: &InputContent) -> Int {
+    let (nb, rem) = compute_scores(nbs, false);
+    assert!(rem.is_empty());
+    nb
 }
 
 fn main() {
@@ -58,7 +67,7 @@ fn main() {
     assert_eq!(res, 40848);
     let res2 = part2(&data);
     println!("{:?}", res2);
-    assert_eq!(res2, 0);
+    assert_eq!(res2, 34466);
     println!("Elapsed time: {:.2?}", before.elapsed());
 }
 
@@ -75,6 +84,6 @@ mod tests {
 
     #[test]
     fn test_part2() {
-        assert_eq!(part2(&get_input_from_str(EXAMPLE)), 0);
+        assert_eq!(part2(&get_input_from_str(EXAMPLE)), 66);
     }
 }
