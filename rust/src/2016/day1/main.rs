edf86c0489f3d1258afb77cbac43df5859d2554a
SylvainDe <sylvain.desodt+github@gmail.com>
2022-09-10 15:44:26 +0200

    Rust - 2016 - Day 9 - Part 2

--- rust/src/2016/day1/main.rs
+++ rust/src/2016/day1/main.rs
@@ -1,6 +1,7 @@
 use common::get_file_content;
 use common::get_first_line;
 use core::str::FromStr;
+use std::collections::HashSet;
 use std::time::Instant;
 
 const INPUT_FILEPATH: &str = "../resources/year2016_day1_input.txt";
@@ -58,7 +59,6 @@ fn get_input_from_file(filepath: &str) -> InputContent {
     get_input_from_str(&get_file_content(filepath))
 }
 
-#[allow(clippy::missing_const_for_fn)]
 fn part1(arg: &InputContent) -> Int {
     let mut x: Int = 0;
     let mut y: Int = 0;
@@ -75,8 +75,27 @@ fn part1(arg: &InputContent) -> Int {
     x.abs() + y.abs()
 }
 
-#[allow(clippy::missing_const_for_fn)]
-fn part2(_arg: &InputContent) -> Int {
+fn part2(arg: &InputContent) -> Int {
+    let mut x: Int = 0;
+    let mut y: Int = 0;
+    let mut dx: Int = 0;
+    let mut dy: Int = 1;
+    let mut positions = HashSet::<(Int, Int)>::new();
+    positions.insert((x, y));
+    for Action { turn, length } in arg {
+        match turn {
+            Turn::Left => (dx, dy) = (-dy, dx),
+            Turn::Right => (dx, dy) = (dy, -dx),
+        };
+        for _i in 0..*length {
+            x += dx;
+            y += dy;
+            if positions.contains(&(x, y)) {
+                return x.abs() + y.abs();
+            }
+            positions.insert((x, y));
+        }
+    }
     0
 }
 
@@ -88,15 +107,13 @@ fn main() {
     assert_eq!(res, 226);
     let res2 = part2(&data);
     println!("{:?}", res2);
-    assert_eq!(res2, 0);
+    assert_eq!(res2, 79);
     println!("Elapsed time: {:.2?}", before.elapsed());
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    const EXAMPLE: &str = "R5, L5, R5, R3";
 
     #[test]
     fn test_action_from_str() {
@@ -111,11 +128,13 @@ mod tests {
 
     #[test]
     fn test_part1() {
+        const EXAMPLE: &str = "R5, L5, R5, R3";
         assert_eq!(part1(&get_input_from_str(EXAMPLE)), 12);
     }
 
     #[test]
     fn test_part2() {
-        assert_eq!(part2(&get_input_from_str(EXAMPLE)), 0);
+        const EXAMPLE: &str = "R8, R4, R4, R8";
+        assert_eq!(part2(&get_input_from_str(EXAMPLE)), 4);
     }
 }
