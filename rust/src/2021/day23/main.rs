f93c1f5809d46d819724d4015cd66ad2ab37172b
SylvainDe <sylvain.desodt+github@gmail.com>
2022-10-24 15:55:50 +0200

    Add missing templates

---
+++ rust/src/2021/day23/main.rs
@@ -0,0 +1,55 @@
+use common::collect_lines;
+use common::get_file_content;
+use std::time::Instant;
+
+const INPUT_FILEPATH: &str = "../resources/year2021_day23_input.txt";
+
+type Int = u32;
+type InputContent = Vec<String>;
+
+fn get_input_from_str(string: &str) -> InputContent {
+    collect_lines(string)
+}
+
+fn get_input_from_file(filepath: &str) -> InputContent {
+    get_input_from_str(&get_file_content(filepath))
+}
+
+#[allow(clippy::missing_const_for_fn)]
+fn part1(_arg: &InputContent) -> Int {
+    0
+}
+
+#[allow(clippy::missing_const_for_fn)]
+fn part2(_arg: &InputContent) -> Int {
+    0
+}
+
+fn main() {
+    let before = Instant::now();
+    let data = get_input_from_file(INPUT_FILEPATH);
+    let res = part1(&data);
+    println!("{:?}", res);
+    assert_eq!(res, 0);
+    let res2 = part2(&data);
+    println!("{:?}", res2);
+    assert_eq!(res2, 0);
+    println!("Elapsed time: {:.2?}", before.elapsed());
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const EXAMPLE: &str = "";
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(part1(&get_input_from_str(EXAMPLE)), 0);
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2(&get_input_from_str(EXAMPLE)), 0);
+    }
+}
