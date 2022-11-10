76e06c0ffeed701d1b5b50948df2c5ae8b977564
SylvainDe <sylvain.desodt+github@gmail.com>
2022-11-10 19:07:25 +0100

    Rust - 2016 - Day 14 - Part 2

--- rust/src/2016/day14/main.rs
+++ rust/src/2016/day14/main.rs
@@ -4,34 +4,19 @@ use std::time::Instant;
 
 const INPUT_FILEPATH: &str = "../resources/year2016_day14_input.txt";
 
-type Int = u32;
 const SKIP_SLOW: bool = true;
 
 fn get_input_from_file(filepath: &str) -> String {
     get_first_line_from_file(filepath)
 }
 
-fn get_hash(salt: &str, index: Int, stretched: bool) -> String {
+fn get_hash(salt: &str, index: usize, stretched: bool) -> String {
     let nb = if stretched { 2017 } else { 1 };
     let mut s = format!("{}{}", salt, index);
     for _ in 0..nb {
         s = format!("{:?}", md5::compute(s));
     }
     s
-}
-
-fn find_triplet(s: &str) -> Option<char> {
-    s.chars()
-        .tuple_windows()
-        .filter(|(a, b, c)| a == b && b == c)
-        .map(|(a, _, _)| a)
-        .next()
-}
-
-fn find_5_repetitions(s: &str, repeated: char) -> bool {
-    s.chars()
-        .tuple_windows()
-        .any(|(a, b, c, d, e)| repeated == a && a == b && b == c && c == d && d == e)
 }
 
 fn find_repetitions(s: &str) -> Vec<(char, usize)> {
@@ -48,51 +33,58 @@ fn find_repetitions(s: &str) -> Vec<(char, usize)> {
         .collect()
 }
 
-fn produce_key(salt: &str, index_wanted: Int, stretched: bool) -> Int {
+fn produce_key(salt: &str, index_wanted: usize, stretched: bool) -> usize {
+    let mut reps = Vec::new();
     let mut nb_found = 0;
-    // Naive implementation - everything is computed so many times :(
-    // A better implementation would compute find_repetitions(&get_hash(salt, i, stretched))
-    // only per index
+    for i in 0..1000 {
+        reps.push(find_repetitions(&get_hash(salt, i, stretched)));
+    }
     for i in 0.. {
-        if let Some(c) = find_triplet(&get_hash(salt, i, stretched)) {
-            let mut found = false;
-            for j in i + 1..=i + 1000 {
-                if find_5_repetitions(&get_hash(salt, j, stretched), c) {
-                    found = true;
-                    break;
+        reps.push(find_repetitions(&get_hash(
+            salt,
+            (i + 1000) as usize,
+            stretched,
+        )));
+        for (c, cnt) in reps.get(i).unwrap() {
+            if cnt >= &3 {
+                'jloop: for j in i + 1..=i + 1000 {
+                    for (c2, cnt2) in reps.get(j).unwrap() {
+                        if c == c2 && cnt2 >= &5 {
+                            nb_found += 1;
+                            if nb_found == index_wanted {
+                                return i;
+                            }
+                            break 'jloop;
+                        }
+                    }
                 }
-            }
-            if found {
-                nb_found += 1;
-                if nb_found == index_wanted {
-                    return i;
-                }
+                break;
             }
         }
     }
     0
 }
 
-fn part1(salt: &str) -> Int {
+fn part1(salt: &str) -> usize {
     produce_key(salt, 64, false)
 }
 
-#[allow(clippy::missing_const_for_fn)]
-fn part2(_arg: &str) -> Int {
-    0
+fn part2(salt: &str) -> usize {
+    produce_key(salt, 64, true)
 }
 
 fn main() {
     let before = Instant::now();
     let data = get_input_from_file(INPUT_FILEPATH);
+    dbg!(&data);
+    let res = part1(&data);
+    println!("{:?}", res);
+    assert_eq!(res, 18626);
     if !SKIP_SLOW {
-        let res = part1(&data);
-        println!("{:?}", res);
-        assert_eq!(res, 18626);
+        let res2 = part2(&data);
+        println!("{:?}", res2);
+        assert_eq!(res2, 20092);
     }
-    let res2 = part2(&data);
-    println!("{:?}", res2);
-    assert_eq!(res2, 0);
     println!("Elapsed time: {:.2?}", before.elapsed());
 }
 
@@ -100,86 +92,81 @@ fn main() {
 mod tests {
     use super::*;
 
-    const EXAMPLE: &str = "";
+    const SALT: &str = "abc";
 
     #[test]
     fn test_get_hash() {
-        let salt = "abc";
         let stretched = false;
         assert_eq!(
-            get_hash(salt, 0, stretched),
+            get_hash(SALT, 0, stretched),
             "577571be4de9dcce85a041ba0410f29f"
         );
         assert_eq!(
-            get_hash(salt, 18, stretched),
+            get_hash(SALT, 18, stretched),
             "0034e0923cc38887a57bd7b1d4f953df"
         );
         assert_eq!(
-            get_hash(salt, 39, stretched),
+            get_hash(SALT, 39, stretched),
             "347dac6ee8eeea4652c7476d0f97bee5"
         );
         assert_eq!(
-            get_hash(salt, 816, stretched),
+            get_hash(SALT, 816, stretched),
             "3aeeeee1367614f3061d165a5fe3cac3"
         );
         assert_eq!(
-            get_hash(salt, 92, stretched),
+            get_hash(SALT, 92, stretched),
             "ae2e85dd75d63e916a525df95e999ea0"
         );
         assert_eq!(
-            get_hash(salt, 200, stretched),
+            get_hash(SALT, 200, stretched),
             "83501e9109999965af11270ef8d61a4f"
         );
         assert_eq!(
-            get_hash(salt, 22728, stretched),
+            get_hash(SALT, 22728, stretched),
             "26ccc731a8706e0c4f979aeb341871f0"
         );
         let stretched = true;
         assert_eq!(
-            get_hash(salt, 0, stretched),
+            get_hash(SALT, 0, stretched),
             "a107ff634856bb300138cac6568c0f24"
         );
     }
 
     #[test]
-    fn test_find_interesting_hash() {
-        let salt = "abc";
+    fn test_find_repetitions() {
         let stretched = false;
-        assert_eq!(find_triplet(&get_hash(salt, 38, stretched)), None);
-        assert_eq!(find_triplet(&get_hash(salt, 39, stretched)), Some('e'));
         assert_eq!(
-            find_5_repetitions(&get_hash(salt, 40, stretched), 'e'),
-            stretched
-        );
-        assert_eq!(
-            find_5_repetitions(&get_hash(salt, 816, stretched), 'e'),
-            true
-        );
-        assert_eq!(produce_key(salt, 1, stretched), 39);
-        assert_eq!(produce_key(salt, 2, stretched), 92);
-        if !SKIP_SLOW {
-            assert_eq!(produce_key(salt, 64, stretched), 22728);
-        }
-        assert_eq!(
-            find_repetitions(&get_hash(salt, 39, stretched)),
+            find_repetitions(&get_hash(SALT, 39, stretched)),
             vec![('e', 2,), ('e', 3,), ('e', 2,),]
         );
         assert_eq!(
-            find_repetitions(&get_hash(salt, 816, stretched)),
+            find_repetitions(&get_hash(SALT, 816, stretched)),
             vec![('e', 5,),]
         );
     }
 
     #[test]
-    fn test_part1() {
+    fn test_find_interesting_hash() {
+        let stretched = false;
+        assert_eq!(produce_key(SALT, 1, stretched), 39);
+        assert_eq!(produce_key(SALT, 2, stretched), 92);
+        assert_eq!(produce_key(SALT, 64, stretched), 22728);
         if !SKIP_SLOW {
-            let salt = "abc";
-            assert_eq!(part1(salt), 22728);
+            let stretched = true;
+            assert_eq!(produce_key(SALT, 1, stretched), 10);
+            assert_eq!(produce_key(SALT, 64, stretched), 22551);
         }
     }
 
     #[test]
+    fn test_part1() {
+        assert_eq!(part1(SALT), 22728);
+    }
+
+    #[test]
     fn test_part2() {
-        assert_eq!(part2(EXAMPLE), 0);
+        if !SKIP_SLOW {
+            assert_eq!(part2(SALT), 22551);
+        }
     }
 }
