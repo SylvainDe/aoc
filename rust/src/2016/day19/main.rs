9c68b54bb83822f32a2864565025a4d94d48f435
SylvainDe <sylvain.desodt+github@gmail.com>
2022-11-11 22:00:37 +0100

    Rust - 2016 - Day 19 - Part 1

--- rust/src/2016/day19/main.rs
+++ rust/src/2016/day19/main.rs
@@ -1,37 +1,43 @@
-use common::collect_lines;
-use common::get_file_content;
+use common::get_first_line_from_file;
 use std::time::Instant;
 
 const INPUT_FILEPATH: &str = "../resources/year2016_day19_input.txt";
 
 type Int = u32;
-type InputContent = Vec<String>;
 
-fn get_input_from_str(string: &str) -> InputContent {
-    collect_lines(string)
+fn get_input_from_str(string: &str) -> Int {
+    string.parse().unwrap()
 }
 
-fn get_input_from_file(filepath: &str) -> InputContent {
-    get_input_from_str(&get_file_content(filepath))
+fn get_input_from_file(filepath: &str) -> Int {
+    get_input_from_str(&get_first_line_from_file(filepath))
+}
+
+fn part1(size: Int) -> Int {
+    // Naive implementation - "slow" but useful to check assumptions
+    // Could be optimised with https://en.wikipedia.org/wiki/Josephus_problem
+    let mut v = (1..=size).collect::<Vec<Int>>();
+    let mut start = 0;
+    while v.len() > 1 {
+        let l = v.len();
+        v = v.iter().skip(start).step_by(2).copied().collect();
+        start = (start + l) % 2;
+    }
+    *v.first().unwrap()
 }
 
 #[allow(clippy::missing_const_for_fn)]
-fn part1(_arg: &InputContent) -> Int {
-    0
-}
-
-#[allow(clippy::missing_const_for_fn)]
-fn part2(_arg: &InputContent) -> Int {
+fn part2(_arg: Int) -> Int {
     0
 }
 
 fn main() {
     let before = Instant::now();
     let data = get_input_from_file(INPUT_FILEPATH);
-    let res = part1(&data);
+    let res = part1(data);
     println!("{:?}", res);
-    assert_eq!(res, 0);
-    let res2 = part2(&data);
+    assert_eq!(res, 1_834_471);
+    let res2 = part2(data);
     println!("{:?}", res2);
     assert_eq!(res2, 0);
     println!("Elapsed time: {:.2?}", before.elapsed());
@@ -41,15 +47,15 @@ fn main() {
 mod tests {
     use super::*;
 
-    const EXAMPLE: &str = "";
+    const EXAMPLE: &str = "5";
 
     #[test]
     fn test_part1() {
-        assert_eq!(part1(&get_input_from_str(EXAMPLE)), 0);
+        assert_eq!(part1(get_input_from_str(EXAMPLE)), 3);
     }
 
     #[test]
     fn test_part2() {
-        assert_eq!(part2(&get_input_from_str(EXAMPLE)), 0);
+        assert_eq!(part2(get_input_from_str(EXAMPLE)), 0);
     }
 }
