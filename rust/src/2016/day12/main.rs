77ad6d00b7cbcd2b8b3e4c01ce1ce1cafe17f55a
SylvainDe <sylvain.desodt+github@gmail.com>
2022-11-20 19:32:41 +0100

    Rust - 2016 - Move assembunny logic to single place

--- rust/src/2016/day12/main.rs
+++ rust/src/2016/day12/main.rs
@@ -1,119 +1,25 @@
-use common::collect_from_lines;
+use common::assembunny2016::get_input_from_str;
+use common::assembunny2016::run_instructions;
+use common::assembunny2016::Instruction;
 use common::get_file_content;
-use core::str::FromStr;
-use std::collections::HashMap;
 use std::time::Instant;
 
 const INPUT_FILEPATH: &str = "../resources/year2016_day12_input.txt";
 
 type Int = i32;
 
-// TODO: Put in common with assembunny logic for day 23
-#[derive(Debug, Eq, Ord, PartialEq, PartialOrd)]
-enum Instruction {
-    Copy(String, String),
-    Increase(String),
-    Decrease(String),
-    Jump(String, String),
-}
-
-impl FromStr for Instruction {
-    type Err = ();
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let chunks: Vec<&str> = s.split(' ').collect();
-        match chunks.first() {
-            None => (),
-            Some(&cmd) => match cmd {
-                "inc" => {
-                    if chunks.len() == 2 {
-                        return Ok(Self::Increase((*chunks.get(1).unwrap()).to_string()));
-                    }
-                }
-                "dec" => {
-                    if chunks.len() == 2 {
-                        return Ok(Self::Decrease((*chunks.get(1).unwrap()).to_string()));
-                    }
-                }
-                "cpy" => {
-                    if chunks.len() == 3 {
-                        return Ok(Self::Copy(
-                            (*chunks.get(1).unwrap()).to_string(),
-                            (*chunks.get(2).unwrap()).to_string(),
-                        ));
-                    }
-                }
-                "jnz" => {
-                    if chunks.len() == 3 {
-                        return Ok(Self::Jump(
-                            (*chunks.get(1).unwrap()).to_string(),
-                            (*chunks.get(2).unwrap()).to_string(),
-                        ));
-                    }
-                }
-                _ => (),
-            },
-        }
-        Err(())
-    }
-}
-
 type InputContent = Vec<Instruction>;
-
-fn get_input_from_str(string: &str) -> InputContent {
-    collect_from_lines(string, Instruction::from_str)
-}
 
 fn get_input_from_file(filepath: &str) -> InputContent {
     get_input_from_str(&get_file_content(filepath))
 }
 
-#[allow(clippy::cast_sign_loss)]
-fn run_instructions(instructions: &InputContent, c_value: Int) -> Int {
-    let mut env = HashMap::from([
-        ("a".to_string(), 0),
-        ("b".to_string(), 0),
-        ("c".to_string(), c_value),
-        ("d".to_string(), 0),
-    ]);
-    let mut cnt = 0;
-    while let Some(ins) = instructions.get(cnt as usize) {
-        match ins {
-            Instruction::Copy(x, y) => {
-                let val = match x.parse::<Int>() {
-                    Ok(n) => n,
-                    Err(_) => *env.get(x).unwrap(),
-                };
-                env.insert(y.to_string(), val);
-            }
-            Instruction::Increase(x) => {
-                env.entry(x.to_string()).and_modify(|e| *e += 1);
-            }
-            Instruction::Decrease(x) => {
-                env.entry(x.to_string()).and_modify(|e| *e -= 1);
-            }
-            Instruction::Jump(x, y) => {
-                let x = match x.parse::<Int>() {
-                    Ok(n) => n,
-                    Err(_) => *env.get(x).unwrap(),
-                };
-                if x != 0 {
-                    let rel_pos = y.parse::<Int>().unwrap();
-                    cnt += rel_pos;
-                    continue;
-                }
-            }
-        }
-        cnt += 1;
-    }
-    *env.get("a").unwrap()
-}
-
 fn part1(instructions: &InputContent) -> Int {
-    run_instructions(instructions, 0)
+    run_instructions(instructions, 0, 0)
 }
 
 fn part2(instructions: &InputContent) -> Int {
-    run_instructions(instructions, 1)
+    run_instructions(instructions, 0, 1)
 }
 
 fn main() {
@@ -138,31 +44,6 @@ inc a
 dec a
 jnz a 2
 dec a";
-
-    #[test]
-    fn test_instruction_from_str() {
-        assert_eq!(
-            Instruction::from_str("cpy 41 a"),
-            Ok(Instruction::Copy("41".to_string(), "a".to_string()))
-        );
-        assert_eq!(
-            Instruction::from_str("jnz a 2"),
-            Ok(Instruction::Jump("a".to_string(), "2".to_string()))
-        );
-        assert_eq!(
-            Instruction::from_str("inc a"),
-            Ok(Instruction::Increase("a".to_string()))
-        );
-        assert_eq!(
-            Instruction::from_str("dec a"),
-            Ok(Instruction::Decrease("a".to_string()))
-        );
-        assert!(Instruction::from_str("").is_err());
-        assert!(Instruction::from_str("abc").is_err());
-        assert!(Instruction::from_str("cpy").is_err());
-        assert!(Instruction::from_str("cpy 41").is_err());
-        assert!(Instruction::from_str("cpy 41 a b").is_err());
-    }
 
     #[test]
     fn test_part1() {
--- rust/src/lib/common.rs
+++ rust/src/lib/common.rs
@@ -73,7 +73,7 @@ pub mod point_module {
 }
 
 #[cfg(test)]
-mod tests {
+mod tests_point {
     use super::*;
     use crate::point_module::Point;
     use std::str::FromStr;
@@ -120,5 +120,215 @@ mod tests {
         assert!(Point::<i32>::from_str("9 4").is_err());
         assert!(Point::<i32>::from_str("9,4").is_err());
         assert!(Point::<i32>::from_str("9,four").is_err());
+    }
+}
+
+pub mod assembunny2016 {
+    use crate::collect_from_lines;
+    use std::collections::HashMap;
+    use std::str::FromStr;
+    type Int = i32;
+    type InputContent = Vec<Instruction>;
+
+    // TODO: Put in common with assembunny logic for day 12
+    #[derive(Debug, Eq, Ord, PartialEq, PartialOrd, Clone)]
+    pub enum Instruction {
+        Copy(String, String),
+        Increase(String),
+        Decrease(String),
+        Jump(String, String),
+        Toggle(String),
+    }
+
+    impl FromStr for Instruction {
+        type Err = ();
+        fn from_str(s: &str) -> Result<Self, Self::Err> {
+            let chunks: Vec<&str> = s.split(' ').collect();
+            match chunks.first() {
+                None => (),
+                Some(&cmd) => match cmd {
+                    "inc" => {
+                        if chunks.len() == 2 {
+                            return Ok(Self::Increase((*chunks.get(1).unwrap()).to_string()));
+                        }
+                    }
+                    "dec" => {
+                        if chunks.len() == 2 {
+                            return Ok(Self::Decrease((*chunks.get(1).unwrap()).to_string()));
+                        }
+                    }
+                    "tgl" => {
+                        if chunks.len() == 2 {
+                            return Ok(Self::Toggle((*chunks.get(1).unwrap()).to_string()));
+                        }
+                    }
+                    "cpy" => {
+                        if chunks.len() == 3 {
+                            return Ok(Self::Copy(
+                                (*chunks.get(1).unwrap()).to_string(),
+                                (*chunks.get(2).unwrap()).to_string(),
+                            ));
+                        }
+                    }
+                    "jnz" => {
+                        if chunks.len() == 3 {
+                            return Ok(Self::Jump(
+                                (*chunks.get(1).unwrap()).to_string(),
+                                (*chunks.get(2).unwrap()).to_string(),
+                            ));
+                        }
+                    }
+                    _ => (),
+                },
+            }
+            Err(())
+        }
+    }
+
+    #[must_use]
+    pub fn get_input_from_str(string: &str) -> InputContent {
+        collect_from_lines(string, Instruction::from_str)
+    }
+
+    #[allow(clippy::missing_const_for_fn)]
+    fn toggle_ins(ins: Instruction) -> Instruction {
+        match ins {
+            // For one-argument instructions, inc becomes dec, and all other one-argument instructions become inc
+            Instruction::Increase(x) => Instruction::Decrease(x),
+            Instruction::Decrease(x) | Instruction::Toggle(x) => Instruction::Increase(x),
+            // For two-argument instructions, jnz becomes cpy, and all other two-instructions become jnz
+            Instruction::Jump(x, y) => Instruction::Copy(x, y),
+            Instruction::Copy(x, y) => Instruction::Jump(x, y),
+        }
+    }
+
+    fn eval_string(s: &str, env: &HashMap<String, Int>) -> Int {
+        match s.parse::<Int>() {
+            Ok(n) => n,
+            Err(_) => *env.get(s).unwrap(),
+        }
+    }
+
+    /// # Panics
+    ///
+    /// Will panic if something goes wrong
+    #[must_use]
+    #[allow(clippy::cast_sign_loss)]
+    pub fn run_instructions(instructions: &InputContent, a_value: Int, c_value: Int) -> Int {
+        let mut instructions = instructions.clone();
+        let mut env = HashMap::from([
+            ("a".to_string(), a_value),
+            ("b".to_string(), 0),
+            ("c".to_string(), c_value),
+            ("d".to_string(), 0),
+        ]);
+        let mut cnt = 0;
+        while let Some(ins) = instructions.get(cnt as usize) {
+            match ins {
+                Instruction::Copy(x, y) => {
+                    let x = eval_string(x, &env);
+                    env.insert(y.to_string(), x);
+                }
+                Instruction::Increase(x) => {
+                    env.entry(x.to_string()).and_modify(|e| *e += 1);
+                }
+                Instruction::Decrease(x) => {
+                    env.entry(x.to_string()).and_modify(|e| *e -= 1);
+                }
+                Instruction::Toggle(x) => {
+                    let x = eval_string(x, &env);
+                    let pos = (cnt + x) as usize;
+                    if let Some(ins2) = instructions.get(pos) {
+                        instructions[pos] = toggle_ins(ins2.clone());
+                    }
+                }
+                Instruction::Jump(x, y) => {
+                    let x = eval_string(x, &env);
+                    if x != 0 {
+                        let y = eval_string(y, &env);
+                        cnt += y;
+                        continue;
+                    }
+                }
+            }
+            cnt += 1;
+        }
+        *env.get("a").unwrap()
+    }
+}
+
+#[cfg(test)]
+mod tests_assembunny2016 {
+    use crate::assembunny2016::get_input_from_str;
+    use crate::assembunny2016::run_instructions;
+    use crate::assembunny2016::Instruction;
+    use core::str::FromStr;
+
+    const EXAMPLE_DAY_12: &str = "cpy 41 a
+inc a
+inc a
+dec a
+jnz a 2
+dec a";
+
+    const EXAMPLE_DAY_23: &str = "cpy 2 a
+tgl a
+tgl a
+tgl a
+cpy 1 a
+dec a
+dec a";
+
+    #[test]
+    fn test_instruction_from_str() {
+        assert_eq!(
+            Instruction::from_str("cpy 41 a"),
+            Ok(Instruction::Copy("41".to_string(), "a".to_string()))
+        );
+        assert_eq!(
+            Instruction::from_str("jnz a 2"),
+            Ok(Instruction::Jump("a".to_string(), "2".to_string()))
+        );
+        assert_eq!(
+            Instruction::from_str("inc a"),
+            Ok(Instruction::Increase("a".to_string()))
+        );
+        assert_eq!(
+            Instruction::from_str("dec a"),
+            Ok(Instruction::Decrease("a".to_string()))
+        );
+        assert_eq!(
+            Instruction::from_str("tgl a"),
+            Ok(Instruction::Toggle("a".to_string()))
+        );
+        assert!(Instruction::from_str("").is_err());
+        assert!(Instruction::from_str("abc").is_err());
+        assert!(Instruction::from_str("cpy").is_err());
+        assert!(Instruction::from_str("cpy 41").is_err());
+        assert!(Instruction::from_str("cpy 41 a b").is_err());
+    }
+
+    #[test]
+    fn test_day12() {
+        assert_eq!(
+            run_instructions(&get_input_from_str(EXAMPLE_DAY_12), 0, 0),
+            42
+        );
+        assert_eq!(
+            run_instructions(&get_input_from_str(EXAMPLE_DAY_12), 0, 1),
+            42
+        );
+    }
+
+    #[test]
+    fn test_day23() {
+        assert_eq!(
+            run_instructions(&get_input_from_str(EXAMPLE_DAY_23), 7, 0),
+            3
+        );
+        assert_eq!(
+            run_instructions(&get_input_from_str(EXAMPLE_DAY_23), 12, 0),
+            3
+        );
     }
 }
