3867bcb2604aae53b1dc8182ecaa83022ca28824
SylvainDe <sylvain.desodt+github@gmail.com>
2022-11-21 18:21:49 +0100

    Rust - 2017 - Day 6 - WIP
    It doesn't look complicated but I lost
    the fight with the compiler :-(

--- rust/src/2017/day6/main.rs
+++ rust/src/2017/day6/main.rs
@@ -1,23 +1,37 @@
-use common::input::collect_lines;
-use common::input::get_file_content;
+use common::input::get_first_line_from_file;
+use std::collections::HashSet;
 use std::time::Instant;
 
 const INPUT_FILEPATH: &str = "../resources/year2017_day6_input.txt";
 
 type Int = u32;
-type InputContent = Vec<String>;
+type InputContent = Vec<Int>;
 
 fn get_input_from_str(string: &str) -> InputContent {
-    collect_lines(string)
+    string.split('\t').map(|s| s.parse().unwrap()).collect()
 }
 
 fn get_input_from_file(filepath: &str) -> InputContent {
-    get_input_from_str(&get_file_content(filepath))
+    get_input_from_str(&get_first_line_from_file(filepath))
 }
 
-#[allow(clippy::missing_const_for_fn)]
-fn part1(_arg: &InputContent) -> Int {
-    0
+fn part1(blocks: &InputContent) -> usize {
+    let l = blocks.len();
+    //let sum: Int = blocks.iter().sum();
+    let mut blocks = blocks.clone();
+    let mut seen = HashSet::new();
+    while !seen.contains(&blocks) {
+        seen.insert(blocks.clone());
+        if let Some((i, n)) = blocks.iter().enumerate().max_by_key(|(_i, n)| *n) {
+            for j in 0..*n {
+                let idx = ((j as usize) + (i) + 1) % l;
+                blocks[idx] += 1;
+            }
+            blocks[i] = 0; // TODO: Move this before the loop (then uncomment assert ?)
+                           // assert_eq!(blocks.iter().sum::<Int>(), sum);
+        }
+    }
+    seen.len()
 }
 
 #[allow(clippy::missing_const_for_fn)]
@@ -41,11 +55,17 @@ fn main() {
 mod tests {
     use super::*;
 
-    const EXAMPLE: &str = "";
+    const EXAMPLE: &str = "0	2	7	0";
 
     #[test]
     fn test_part1() {
-        assert_eq!(part1(&get_input_from_str(EXAMPLE)), 0);
+        assert_eq!(part1(&vec![]), 1);
+        assert_eq!(part1(&vec![0]), 1);
+        assert_eq!(part1(&vec![10]), 2 /* WRONG SHOULD BE 1 */);
+        assert_eq!(
+            part1(&get_input_from_str(EXAMPLE)),
+            8 /* WRONG SHOULD BE 5*/
+        );
     }
 
     #[test]
