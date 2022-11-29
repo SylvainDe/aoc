bd1d4db88cfd9d8050229233f9b32edd13a5e81f
SylvainDe <sylvain.desodt+github@gmail.com>
2022-11-29 23:03:52 +0100

    Add rust template for Regex based parsing

---
+++ rust/src/templates/template_regex.rs
@@ -0,0 +1,104 @@
+use common::input::collect_from_lines;
+use common::input::get_file_content;
+use core::str::FromStr;
+use lazy_static::lazy_static;
+use regex::Regex;
+use std::time::Instant;
+
+const INPUT_FILEPATH: &str = "../resources/yearYYYY_dayDD_input.txt";
+
+type Int = u32;
+
+#[derive(Debug, PartialEq)]
+struct FooBar {
+    char1: char,
+    int2: Int,
+}
+
+impl FromStr for FooBar {
+    type Err = ();
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        // TODO: Example of the regexp
+        lazy_static! {
+            static ref RE: Regex = Regex::new(r"^(?P<char1>.) (?P<int2>\d+)$").unwrap();
+        }
+        let c = RE.captures(s).ok_or(())?;
+        Ok(Self {
+            char1: c
+                .name("char1")
+                .ok_or(())?
+                .as_str()
+                .chars()
+                .next()
+                .ok_or(())?,
+            int2: c
+                .name("int2")
+                .ok_or(())?
+                .as_str()
+                .parse::<Int>()
+                .map_err(|_| {})?,
+        })
+    }
+}
+
+type InputContent = Vec<FooBar>;
+
+fn get_input_from_str(string: &str) -> InputContent {
+    collect_from_lines(string)
+}
+
+fn get_input_from_file(filepath: &str) -> InputContent {
+    get_input_from_str(&get_file_content(filepath))
+}
+
+#[allow(clippy::missing_const_for_fn)]
+fn part1(_arg: &InputContent) -> Int {
+    0
+}
+
+#[allow(clippy::missing_const_for_fn)]
+fn part2(_arg: &InputContent) -> Int {
+    0
+}
+
+fn main() {
+    let before = Instant::now();
+    let data = get_input_from_file(INPUT_FILEPATH);
+    let res = part1(&data);
+    println!("{:?}", res);
+    assert_eq!(res, 0);
+    let res2 = part2(&data);
+    println!("{:?}", res2);
+    assert_eq!(res2, 0);
+    println!("Elapsed time: {:.2?}", before.elapsed());
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const EXAMPLE: &str = "C 1
+C 2
+A 3";
+
+    #[test]
+    fn test_foobar_from_str() {
+        assert!(FooBar::from_str("").is_err());
+        assert_eq!(
+            FooBar::from_str("C 42"),
+            Ok(FooBar {
+                char1: 'C',
+                int2: 42
+            })
+        );
+    }
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(part1(&get_input_from_str(EXAMPLE)), 0);
+    }
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2(&get_input_from_str(EXAMPLE)), 0);
+    }
+}
