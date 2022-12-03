0ef6cfcbc82e64dd8f3a7cb26d9610f7f386cd3d
SylvainDe <sylvain.desodt+github@gmail.com>
2022-12-03 08:30:39 +0100

    Rust - 2022 - Day 3 - Part 2

--- rust/src/2022/day3.rs
+++ rust/src/2022/day3.rs
@@ -12,6 +12,16 @@ fn get_input_from_str(string: &str) -> InputContent {
     collect_lines(string)
 }
 
+fn get_char_prio(c: char) -> Int {
+    if c.is_lowercase() {
+        (c as Int) - ('a' as Int) + 1
+    } else if c.is_uppercase() {
+        (c as Int) - ('A' as Int) + 27
+    } else {
+        panic!("Unexpected char");
+    }
+}
+
 fn double_compartment_priority(s: &String) -> Int {
     let len = s.len();
     assert_eq!(len % 2, 0);
@@ -22,22 +32,31 @@ fn double_compartment_priority(s: &String) -> Int {
     let s2 = s2.chars().collect::<HashSet<char>>();
     let mut intersect = s1.intersection(&s2);
     let item = intersect.next().unwrap();
-    if item.is_lowercase() {
-        (*item as Int) - ('a' as Int) + 1
-    } else if item.is_uppercase() {
-        (*item as Int) - ('A' as Int) + 27
-    } else {
-        panic!("Unexpected char");
-    }
+    get_char_prio(*item)
 }
 
 fn part1(rucksacks: &InputContent) -> Int {
     rucksacks.iter().map(double_compartment_priority).sum()
 }
 
-#[allow(clippy::missing_const_for_fn)]
-fn part2(_arg: &InputContent) -> Int {
-    0
+fn badge_prio(s1: &str, s2: &str, s3: &str) -> Int {
+    let s1 = s1.chars().collect::<HashSet<char>>();
+    let s2 = s2.chars().collect::<HashSet<char>>();
+    let s3 = s3.chars().collect::<HashSet<char>>();
+    let intersect = s1.intersection(&s2).copied().collect::<HashSet<char>>();
+    let intersect = s3
+        .intersection(&intersect)
+        .copied()
+        .collect::<HashSet<char>>();
+    let item = intersect.iter().next().unwrap();
+    get_char_prio(*item)
+}
+
+fn part2(rucksacks: &InputContent) -> Int {
+    rucksacks
+        .chunks(3)
+        .map(|s| badge_prio(&s[0], &s[1], &s[2]))
+        .sum()
 }
 
 fn main() {
@@ -48,7 +67,7 @@ fn main() {
     assert_eq!(res, 7889);
     let res2 = part2(&data);
     println!("{:?}", res2);
-    assert_eq!(res2, 0);
+    assert_eq!(res2, 2825);
     println!("Elapsed time: {:.2?}", before.elapsed());
 }
 
@@ -60,6 +79,11 @@ mod tests {
 
     #[test]
     fn test_double_compartment_priority() {
+        // TODO
+    }
+
+    #[test]
+    fn test_badge_priority() {
         // TODO
     }
 
