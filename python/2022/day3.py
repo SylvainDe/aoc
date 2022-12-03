ce5c3b34c76b9edd1f27f6b07b6c5f756af6f455
SylvainDe <sylvain.desodt+github@gmail.com>
2022-12-03 12:19:31 +0100

    Python - 2022 - Day 3

--- python/2022/day3.py
+++ python/2022/day3.py
@@ -2,17 +2,52 @@
 import datetime
 
 
-def get_xxx_from_file(file_path="../../resources/year2022_day3_input.txt"):
+def get_rucksacks_from_file(file_path="../../resources/year2022_day3_input.txt"):
     with open(file_path) as f:
         return [l.strip() for l in f]
 
+def get_prio(char):
+    if char.islower():
+        return ord(char) - ord('a') + 1
+    if char.isupper():
+        return ord(char) - ord('A') + 27
+    assert False
+
+
+def get_set_prio(sets):
+    intersect = set.intersection(*sets)
+    assert len(intersect) == 1
+    return get_prio(intersect.pop())
+
+
+def double_compartment_priority(rucksack):
+    l = len(rucksack)
+    q, r = divmod(l, 2)
+    assert r == 0
+    s1, s2 = rucksack[:q], rucksack[q:]
+    assert len(s1) == len(s2) == q
+    assert rucksack == s1 + s2
+    return get_set_prio((set(s1), s2))
+
+
+def part1(rucksacks):
+   return sum(double_compartment_priority(r) for r in rucksacks)
+
+
+def part2(rucksacks):
+    # Inspired from itertools recipes
+    args = [iter(rucksacks)] * 3
+    return sum(get_set_prio((set(s1), s2, s3)) for (s1, s2, s3) in zip(*args))
+
 
 def run_tests():
-    xxx = ""
+    rucksacks = ""
 
 
 def get_solutions():
-    xxx = get_xxx_from_file()
+    rucksacks = get_rucksacks_from_file()
+    print(part1(rucksacks) == 7889)
+    print(part2(rucksacks) == 2825)
 
 
 if __name__ == "__main__":
