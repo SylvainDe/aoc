28d685fc0ebf086af4fb21a784f3c639ff31d29f
SylvainDe <sylvain.desodt+github@gmail.com>
2022-12-04 10:11:11 +0100

    Python - 2022 - Day 4

--- python/2022/day4.py
+++ python/2022/day4.py
@@ -2,26 +2,40 @@
 import datetime
 
 
-def get_xxx_from_line(string):
-    return string
+def get_ids_from_line(string):
+    id1, id2 = string.split(",")
+    return tuple(int(n) for n in id1.split("-") + id2.split("-"))
 
-def get_xxx_from_lines(string):
-    return [get_xxx_from_line(l) for l in string.splitlines()]
+def get_ids_from_lines(string):
+    return [get_ids_from_line(l) for l in string.splitlines()]
 
-def get_xxx_from_file(file_path="../../resources/year2022_day4_input.txt"):
+def get_ids_from_file(file_path="../../resources/year2022_day4_input.txt"):
     with open(file_path) as f:
-        return get_xxx_from_lines(f.read())
+        return get_ids_from_lines(f.read())
 
+def nb_fully_contains(ids):
+    return sum((a <= c and d <= b) or
+               (c <= a and b <= d)
+               for a, b, c, d in ids)
+
+def nb_overlap(ids):
+    return sum(not((a > d) or (c > b)) for a, b, c, d in ids)
 
 def run_tests():
-    xxx = get_xxx_from_lines("""abc
-def
-ghi""")
+    ids = get_ids_from_lines("""2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8""")
+    assert nb_fully_contains(ids) == 2
+    assert nb_overlap(ids) == 4
 
 
 def get_solutions():
-    xxx = get_xxx_from_file()
-
+    ids = get_ids_from_file()
+    print(nb_fully_contains(ids) == 518)
+    print(nb_overlap(ids) == 909)
 
 if __name__ == "__main__":
     begin = datetime.datetime.now()
