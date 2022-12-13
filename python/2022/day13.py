84a724476bafa2e23140e5cdbf7567abe7c336a5
SylvainDe <sylvain.desodt+github@gmail.com>
2022-12-13 08:58:23 +0100

    Python - 2022 - Day 13 - Part 1

--- python/2022/day13.py
+++ python/2022/day13.py
@@ -2,26 +2,69 @@
 import datetime
 
 
-def get_xxx_from_line(string):
-    return string
+def get_packets_from_line(string):
+    p1, p2 = string.split("\n")
+    return (eval(p1), eval(p2))
 
-def get_xxx_from_lines(string):
-    return [get_xxx_from_line(l) for l in string.splitlines()]
+def get_packets_from_lines(string):
+    return [get_packets_from_line(l.strip()) for l in string.split("\n\n")]
 
-def get_xxx_from_file(file_path="../../resources/year2022_day13_input.txt"):
+def get_packets_from_file(file_path="../../resources/year2022_day13_input.txt"):
     with open(file_path) as f:
-        return get_xxx_from_lines(f.read())
+        return get_packets_from_lines(f.read())
 
+def right_order(p1, p2):
+    t1, t2 = type(p1), type(p2)
+    if (t1, t2) == (int, int):
+        if p1 != p2:
+            return p1 < p2
+    elif (t1, t2) == (list, list):
+        for v1, v2 in zip(p1, p2):
+            order = right_order(v1, v2)
+            if order is not None:
+                return order
+        l1, l2 = len(p1), len(p2)
+        if l1 != l2:
+            return l1 < l2
+    elif (t1, t2) == (list, int):
+        return right_order(p1, [p2])
+    elif (t1, t2) == (int, list):
+        return right_order([p1], p2)
+    return None
+
+def part1(packets):
+    return sum(i for (i, (p1, p2)) in enumerate(packets, start=1) if right_order(p1, p2))
 
 def run_tests():
-    xxx = get_xxx_from_lines("""abc
-def
-ghi""")
+    packets = get_packets_from_lines("""[1,1,3,1,1]
+[1,1,5,1,1]
+
+[[1],[2,3,4]]
+[[1],4]
+
+[9]
+[[8,7,6]]
+
+[[4,4],4,4]
+[[4,4],4,4,4]
+
+[7,7,7,7]
+[7,7,7]
+
+[]
+[3]
+
+[[[]]]
+[[]]
+
+[1,[2,[3,[4,[5,6,7]]]],8,9]
+[1,[2,[3,[4,[5,6,0]]]],8,9]""")
+    print(part1(packets))
 
 
 def get_solutions():
-    xxx = get_xxx_from_file()
-
+    packets = get_packets_from_file()
+    print(part1(packets))
 
 if __name__ == "__main__":
     begin = datetime.datetime.now()
