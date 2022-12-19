949890173ccd099393f9250cbc6bf831313aea20
SylvainDe <sylvain.desodt+github@gmail.com>
2022-12-19 09:10:45 +0100

    Year 2022 - Day 19 - Parsing

--- python/2022/day19.py
+++ python/2022/day19.py
@@ -1,27 +1,30 @@
 # vi: set shiftwidth=4 tabstop=4 expandtab:
 import datetime
+import re
+import collections
 
+# Blueprint 1: Each ore robot costs 3 ore. Each clay robot costs 4 ore. Each obsidian robot costs 4 ore and 18 clay. Each geode robot costs 3 ore and 8 obsidian.
+blueprint_re = re.compile("^Blueprint (\d+): Each ore robot costs (\d+) ore. Each clay robot costs (\d+) ore. Each obsidian robot costs (\d+) ore and (\d+) clay. Each geode robot costs (\d+) ore and (\d+) obsidian.$")
 
-def get_xxx_from_line(string):
-    return string
+Blueprint = collections.namedtuple("Blueprint", ["id", "ore_robot_ore", "clay_robot_ore", "obsidian_robot_ore", "obsidian_robot_clay", "geode_robot_ore", "geode_robot_obsidian"])
 
-def get_xxx_from_lines(string):
-    return [get_xxx_from_line(l) for l in string.splitlines()]
+def get_blueprint_from_line(string):
+    return Blueprint(*[int(s) for s in blueprint_re.match(string).groups()])
 
-def get_xxx_from_file(file_path="../../resources/year2022_day19_input.txt"):
+def get_blueprints_from_lines(string):
+    return [get_blueprint_from_line(l) for l in string.splitlines()]
+
+def get_blueprints_from_file(file_path="../../resources/year2022_day19_input.txt"):
     with open(file_path) as f:
-        return get_xxx_from_lines(f.read())
-
+        return get_blueprints_from_lines(f.read())
 
 def run_tests():
-    xxx = get_xxx_from_lines("""abc
-def
-ghi""")
+    blueprints = get_blueprints_from_lines("""Blueprint 1: Each ore robot costs 4 ore. Each clay robot costs 2 ore. Each obsidian robot costs 3 ore and 14 clay. Each geode robot costs 2 ore and 7 obsidian.
+Blueprint 2: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 8 clay. Each geode robot costs 3 ore and 12 obsidian.""")
 
 
 def get_solutions():
-    xxx = get_xxx_from_file()
-
+    blueprints = get_blueprints_from_file()
 
 if __name__ == "__main__":
     begin = datetime.datetime.now()
