33a547e42761447d255d315eb9871ef7a4fa8131
SylvainDe <sylvain.desodt+github@gmail.com>
2022-12-19 21:03:55 +0100

    Year 2022 - Day 19 - Part 2
    Optimation inspired from reddit

--- python/2022/day19.py
+++ python/2022/day19.py
@@ -2,6 +2,8 @@
 import datetime
 import re
 import collections
+import operator
+import functools
 
 SKIP_SLOW = True
 
@@ -20,7 +22,7 @@ def get_blueprints_from_file(file_path="../../resources/year2022_day19_input.txt
     with open(file_path) as f:
         return get_blueprints_from_lines(f.read())
 
-def get_max_geodes(bp, time=24):
+def get_max_geodes(bp, time):
     queue = collections.deque([(time, 0, 0, 0, 0, 1, 0, 0, 0)]) # time, ore, clay, obsidian, geode, ore_robot, clay_robot, obsidian_robot, geode_robot
     max_geode = 0
     seen = set()
@@ -28,18 +30,24 @@ def get_max_geodes(bp, time=24):
     max_ore_cost = max(bp.ore_robot_ore, bp.clay_robot_ore, bp.obsidian_robot_ore, bp.geode_robot_ore)
     max_clay_cost = bp.obsidian_robot_clay
     max_obs_cost = bp.geode_robot_obsidian
-    # prev_time = None
+    prev_time = None
     while queue:
         state = queue.popleft()
         assert all(v >= 0 for v in state)
         time, ore, clay, obsidian, geode, ore_robot, clay_robot, obsidian_robot, geode_robot = state
+        # if time != prev_time:
+        #     print(bp.id, "time:", time, ":", len(queue), ",", max_geode)
+        # prev_time = time
         max_geode = max(geode, max_geode)
         if time == 0:
             continue
-        # if time != prev_time:
-        #     print(bp.id, "time:", time, ":", len(queue), ",", max_geode)
-        prev_time = time
         time2 = time - 1
+        # Get rid of useless resources
+        # Inspired from https://github.com/alexander-yu/adventofcode/blob/master/problems_2022/19.py
+        ore = min(ore, max_ore_cost + (max_ore_cost - ore_robot) * time2)
+        clay = min(clay, max_clay_cost + (max_clay_cost - clay_robot) * time2)
+        obsidian = min(obsidian, max_obs_cost + (max_obs_cost - obsidian_robot) * time2)
+        state = (time, ore, clay, obsidian, geode, ore_robot, clay_robot, obsidian_robot, geode_robot)
         if state in seen:
             continue
         seen.add(state)
@@ -61,25 +69,39 @@ def get_max_geodes(bp, time=24):
     return max_geode
 
 def get_quality_level(bp):
-    return bp.id * get_max_geodes(bp)
+    return bp.id * get_max_geodes(bp, time=24)
 
 def get_quality_levels(blueprints):
     return sum(get_quality_level(bp) for bp in blueprints)
 
+def mult(iterable, start=1):
+    """Returns the product of an iterable - like the sum builtin."""
+    return functools.reduce(operator.mul, iterable, start)
+
 def run_tests():
     blueprints = get_blueprints_from_lines("""Blueprint 1: Each ore robot costs 4 ore. Each clay robot costs 2 ore. Each obsidian robot costs 3 ore and 14 clay. Each geode robot costs 2 ore and 7 obsidian.
 Blueprint 2: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 8 clay. Each geode robot costs 3 ore and 12 obsidian.""")
+    bp0, bp1 = blueprints
     if not SKIP_SLOW:
-        assert get_max_geodes(blueprints[0]) == 9
-        assert get_max_geodes(blueprints[1]) == 12
-        assert get_quality_level(blueprints[0]) == 9
-        assert get_quality_level(blueprints[1]) == 24
+        # Shorter tests
+        t = 21
+        assert get_max_geodes(bp0, t) == 3
+        assert get_max_geodes(bp1, t) == 4
+        t = 24
+        assert get_max_geodes(bp0, t) == 9
+        assert get_max_geodes(bp1, t) == 12
+        assert get_quality_level(bp0) == 9
+        assert get_quality_level(bp1) == 24
         assert get_quality_levels(blueprints) == 33
+        t = 32
+        assert get_max_geodes(bp0, t) == 56
+        assert get_max_geodes(bp1, t) == 62
 
 def get_solutions():
     blueprints = get_blueprints_from_file()
     if not SKIP_SLOW:
         print(get_quality_levels(blueprints) == 960)
+        print(mult(get_max_geodes(bp, time=32) for bp in blueprints[:3]) == 2040)
 
 if __name__ == "__main__":
     begin = datetime.datetime.now()
