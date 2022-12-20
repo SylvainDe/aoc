83804a88d144547cec19358960e6005fa0af3826
SylvainDe <sylvain.desodt+github@gmail.com>
2022-12-20 13:47:50 +0100

    Python - 2022 - Day 20 - cleanup
    Improve tests and check that the value 0 moves
    the way we expect it to move (even though it
    does not change final result).

--- python/2022/day20.py
+++ python/2022/day20.py
@@ -10,26 +10,20 @@ def get_numbers_from_file(file_path="../../resources/year2022_day20_input.txt"):
         return get_numbers_from_lines(f.read())
 
 def move(src, shift, lst):
-    mod = len(lst) -1
-    dst = ((src + shift - 1) % mod) + 1
-    val = lst.pop(src)
-    lst.insert(dst, val)
-    return lst # Only to make testing easier
+    if shift:
+        mod = len(lst) - 1
+        dst = ((src + shift - 1) % mod) + 1
+        val = lst.pop(src)
+        lst.insert(dst, val)
 
-def mix(numbers, key, nb_mix):
-    numbers = [key * n for n in numbers]
+def mix(numbers, nb_mix):
     # Add index to numbers for tracking
     indexed_numbers = list(enumerate(numbers))
     mixed_numbers = list(indexed_numbers)
     assert id(indexed_numbers) != id(mixed_numbers)
     for val in indexed_numbers * nb_mix:
-        src = mixed_numbers.index(val)
-        move(src, val[1], mixed_numbers)
-    # TODO: The mixing logic looks wrong on the example provided but
-    # the result is not affected...
-    # if len(mixed_numbers) < 10:
-    #     print([v[1] for v in mixed_numbers])
-    return groove([v[1] for v in mixed_numbers])
+        move(mixed_numbers.index(val), val[1], mixed_numbers)
+    return [v[1] for v in mixed_numbers]
 
 def groove(numbers):
     idx = numbers.index(0)
@@ -37,43 +31,40 @@ def groove(numbers):
     return sum(numbers[(i+idx)%l] for i in (1000, 2000, 3000))
 
 def mix1(numbers):
-    return mix(numbers, 1, 1)
+    return groove(mix(numbers, 1))
 
-def mix2(numbers):
-    return mix(numbers, 811589153, 10)
+def mix2(numbers, key=811589153):
+    return groove(mix([n * key for n in numbers], 10))
 
 
 def run_tests():
-    # Small tests
+    # Part 1: Small tests
     lst = [4, 5, 6, 1, 7, 8, 9]
     val = 1
-    assert move(lst.index(val), val, lst) == [4, 5, 6, 7, 1, 8, 9]
+    move(lst.index(val), val, lst)
+    assert lst == [4, 5, 6, 7, 1, 8, 9]
     lst = [4, -2, 5, 6, 7, 8, 9]
     val = -2
-    assert move(lst.index(val), val, lst) == [4, 5, 6, 7, 8, -2, 9]
-    # Big test
-    lst1 = [1, 2, -3, 3, -2, 0, 4]
-    lst2 = [2, 1, -3, 3, -2, 0, 4]
-    lst3 = [1, -3, 2, 3, -2, 0, 4]
-    lst4 = [1, 2, 3, -2, -3, 0, 4]
-    lst5 = [1, 2, -2, -3, 0, 3, 4]
-    lst6 = [1, 2, -3, 0, 3, 4, -2]
-    lst7 = [1, 2, -3, 0, 3, 4, -2]
-    lst8 = [1, 2, -3, 4, 0, 3, -2]
-    val = 1
-    assert move(lst1.index(val), val, lst1) == lst2
-    val = 2
-    assert move(lst2.index(val), val, lst2) == lst3
-    val = -3
-    assert move(lst3.index(val), val, lst3) == lst4
-    val = 3
-    assert move(lst4.index(val), val, lst4) == lst5
-    val = -2
-    assert move(lst5.index(val), val, lst5) == lst6
-    val = 0
-    assert move(lst6.index(val), val, lst6) == lst7
-    val = 4
-    assert move(lst7.index(val), val, lst7) == lst8
+    move(lst.index(val), val, lst)
+    assert lst == [4, 5, 6, 7, 8, -2, 9]
+    # Part 1: Big test step by step
+    steps = [
+        [1, 2, -3, 3, -2, 0, 4],
+        [2, 1, -3, 3, -2, 0, 4],
+        [1, -3, 2, 3, -2, 0, 4],
+        [1, 2, 3, -2, -3, 0, 4],
+        [1, 2, -2, -3, 0, 3, 4],
+        [1, 2, -3, 0, 3, 4, -2],
+        [1, 2, -3, 0, 3, 4, -2],
+        [1, 2, -3, 4, 0, 3, -2],
+    ]
+    initial = steps[0]
+    for i, val in enumerate(initial):
+        l = list(steps[i])
+        move(l.index(val), val, l)
+        assert l == steps[i+1]
+    assert mix(initial, 1) == steps[-1]
+    # Part 1: Whole test
     numbers = get_numbers_from_lines("""1
 2
 -3
@@ -82,6 +73,11 @@ def run_tests():
 0
 4""")
     assert mix1(numbers) == 3
+    # Part 2: Big test step by step
+    lst1 = [811589153, 1623178306, -2434767459, 2434767459, -1623178306, 0, 3246356612]
+    assert mix(lst1, 1) == [0, -2434767459, 3246356612, -1623178306, 2434767459, 1623178306, 811589153]
+    assert mix(lst1, 10) == [0, -2434767459, 1623178306, 3246356612, -1623178306, 2434767459, 811589153]
+    # Part 2: Whole test
     assert mix2(numbers) == 1623178306
 
 def get_solutions():
