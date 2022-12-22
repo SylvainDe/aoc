806bddd5ea2040395dd866c9a10db368f393b2de
SylvainDe <sylvain.desodt+github@gmail.com>
2022-12-22 09:02:13 +0100

    Year 2022 - Day 22 - Getting started

---
+++ python/2022/day22.py
@@ -0,0 +1,31 @@
+# vi: set shiftwidth=4 tabstop=4 expandtab:
+import datetime
+
+
+def get_xxx_from_line(string):
+    return string
+
+def get_xxxs_from_lines(string):
+    return [get_xxx_from_line(l) for l in string.splitlines()]
+
+def get_xxxs_from_file(file_path="../../resources/year2022_day22_input.txt"):
+    with open(file_path) as f:
+        return get_xxxs_from_lines(f.read())
+
+
+def run_tests():
+    xxxs = get_xxxs_from_lines("""abc
+def
+ghi""")
+
+
+def get_solutions():
+    xxxs = get_xxxs_from_file()
+
+
+if __name__ == "__main__":
+    begin = datetime.datetime.now()
+    run_tests()
+    get_solutions()
+    end = datetime.datetime.now()
+    print(end - begin)
