c52f468f6a5e19e8ddad8b9a56786c63302d9059
SylvainDe <sylvain.desodt+github@gmail.com>
2023-12-12 09:39:09 +0100

    Python - 2023 - Day 12 - Part 2 to be optimised

--- python/2023/day12.py
+++ python/2023/day12.py
@@ -43,13 +43,19 @@ def generate_smallest_combination(groups):
     return "".join(ret[:-1])
 
 
+def get_nb_arrangements_wrapper(chars, groups):
+    ret = get_nb_arrangements(chars, groups)
+    print(chars, groups, ret)
+    return ret
+
 def get_nb_arrangements(chars, groups):
     if not groups:
         return 0 if FULL in chars else 1
     if not chars:
         return 0
 
-    min_blocks = sum(groups) + len(groups) - 1
+    sum_groups = sum(groups)
+    min_blocks = sum_groups + len(groups) - 1
     freedom = len(chars) - min_blocks
     if freedom < 0:
         return 0
@@ -62,16 +68,44 @@ def get_nb_arrangements(chars, groups):
             if suffix_matches(expected_beg, chars) else 0
     if first_c == EMPTY:
         return get_nb_arrangements(other_c, groups)
+
     assert first_c == UNKNOWN
-    # if freedom == 0:
-    #     cand = generate_smallest_combination(groups)
-    #     return 1 if suffix_matches(cand, chars) else 0
-    return get_nb_arrangements(FULL + other_c, groups) + get_nb_arrangements(EMPTY + other_c, groups)
+    if chars[-1] != UNKNOWN:
+        return get_nb_arrangements("".join(reversed(chars)), list(reversed(groups)))
+
+    if freedom == 0:
+        cand = generate_smallest_combination(groups)
+        return 1 if suffix_matches(cand, chars) else 0
+
+    # nb_full = sum(c == FULL for c in chars)
+    # nb_unknown = sum(c == UNKNOWN for c in chars)
+    # nb_empty = sum(c == EMPTY for c in chars)
+    # if nb_full == sum_groups:
+    #     cand = chars.replace(UNKNOWN, EMPTY)
+    #     return get_nb_arrangements(cand, groups)
+    # if nb_full > sum_groups:
+    #     return 0
+    # if nb_full + nb_unknown < sum_groups:
+    #     return 0
+    # if nb_full + nb_unknown == sum_groups:
+    #     cand = chars.replace(UNKNOWN, FULL)
+    #     return get_nb_arrangements(cand, groups)
+    ret = get_nb_arrangements(FULL + other_c, groups) + get_nb_arrangements(EMPTY + other_c, groups)
+    # if nb_full + nb_empty == 0:
+    #     # print(chars, groups, freedom, ret)
+    #     pass # There is an optimisation to be found here
+    return ret
 
 
 def get_sum_nb_arrangement(records):
-    return sum(get_nb_arrangements(chars, groups) for chars, groups in records)
+    return sum(get_nb_arrangements_wrapper(chars, groups) for chars, groups in records)
 
+def unfold_record(record, nb):
+    chars, groups = record
+    return ("?".join([chars] *nb), groups * nb)
+
+def unfold_records(records, nb=5):
+    return [unfold_record(r, nb) for r in records]
 
 def run_tests():
     assert generate_smallest_combination([]) == ""
@@ -86,11 +120,13 @@ def run_tests():
 ?###???????? 3,2,1"""
     )
     assert get_sum_nb_arrangement(records) == 21
+    assert get_sum_nb_arrangement(unfold_records(records)) == 525152
 
 
 def get_solutions():
     records = get_records_from_file()
     print(get_sum_nb_arrangement(records) == 6827)
+    # print(get_sum_nb_arrangement(unfold_records(records)))
 
 
 if __name__ == "__main__":
