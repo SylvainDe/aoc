3320d038287bc8ac27aa17e16393d61dda714ece
SylvainDe <sylvain.desodt+github@gmail.com>
2023-12-12 14:43:22 +0100

    Python - 2023 - Day 12 - Part 2

--- README.md
+++ README.md
@@ -23,8 +23,8 @@ Solutions are written in Python and/or Rust.
 |Day 9: Mirage Maintenance|[Problem](https://adventofcode.com/2023/day/9) [Input](https://adventofcode.com/2023/day/9/input)|[puzzle.html](resources/year2023_day9_puzzle.html) [input.txt](resources/year2023_day9_input.txt)|**|[day9.py](python/2023/day9.py)|[-](rust/src/2023/day9.rs)|01:10:31|01:13:35|
 |Day 10: Pipe Maze|[Problem](https://adventofcode.com/2023/day/10) [Input](https://adventofcode.com/2023/day/10/input)|[puzzle.html](resources/year2023_day10_puzzle.html) [input.txt](resources/year2023_day10_input.txt)|**|[day10.py](python/2023/day10.py)|[-](rust/src/2023/day10.rs)|03:23:25|03:58:40|
 |Day 11: Cosmic Expansion|[Problem](https://adventofcode.com/2023/day/11) [Input](https://adventofcode.com/2023/day/11/input)|[puzzle.html](resources/year2023_day11_puzzle.html) [input.txt](resources/year2023_day11_input.txt)|**|[day11.py](python/2023/day11.py)|[-](rust/src/2023/day11.rs)|05:33:29|05:36:45|
-|Day 12: Hot Springs|[Problem](https://adventofcode.com/2023/day/12) [Input](https://adventofcode.com/2023/day/12/input)|[puzzle.html](resources/year2023_day12_puzzle.html) [input.txt](resources/year2023_day12_input.txt)|*|[day12.py](python/2023/day12.py)|[-](rust/src/2023/day12.rs)|03:24:19|-|
-|[2023](https://adventofcode.com/2023)|[Stats](https://adventofcode.com/2023/leaderboard/self)|[stats.txt](misc/leaderboard_self_2023.html)|23|-|-|-|-|
+|Day 12: Hot Springs|[Problem](https://adventofcode.com/2023/day/12) [Input](https://adventofcode.com/2023/day/12/input)|[puzzle.html](resources/year2023_day12_puzzle.html) [input.txt](resources/year2023_day12_input.txt)|**|[day12.py](python/2023/day12.py)|[-](rust/src/2023/day12.rs)|03:24:19|07:46:02|
+|[2023](https://adventofcode.com/2023)|[Stats](https://adventofcode.com/2023/leaderboard/self)|[stats.txt](misc/leaderboard_self_2023.html)|24|-|-|-|-|
 
 ## [2022](https://adventofcode.com/2022)
 |Day|URLs|Puzzle & Input|Stars|Python|Rust|Time part 1|Time part 2|
@@ -280,7 +280,7 @@ Solutions are written in Python and/or Rust.
 |9|**|**|**||**|**|**|**|**|16: ****************|
 |10|**|**|**|**|**|**|**|**|**|18: ******************|
 |11||**|**|**||**|**|**|**|14: **************|
-|12|**|**|**||**|**|**|**|*|15: ***************|
+|12|**|**|**||**|**|**|**|**|16: ****************|
 |13||**|**|||**|**|**||10: **********|
 |14|**|**||**|**|**|**|**||14: **************|
 |15||**||||**|**|**|| 8: ********|
@@ -294,4 +294,4 @@ Solutions are written in Python and/or Rust.
 |23||**||||**|*|**|| 7: *******|
 |24||**|||**|**||**|| 8: ********|
 |25|*|*||*||**|*|**|| 8: ********|
-|Total|30|48|28|23|28|50|46|50|23|326|
+|Total|30|48|28|23|28|50|46|50|24|327|
--- python/2023/day12.py
+++ python/2023/day12.py
@@ -44,9 +44,23 @@ def generate_smallest_combination(groups):
 
 
 def get_nb_arrangements_wrapper(chars, groups):
+    # print(chars, groups)
     ret = get_nb_arrangements(chars, groups)
-    print(chars, groups, ret)
+    # print(chars, groups, ret)
     return ret
+
+import functools
+
+@functools.lru_cache()
+def get_nb_ways_split_number_in_buckets(n, nb_buckets):
+    # TODO: dynamic programming
+    if nb_buckets == 1:
+        return 1
+    if n == 0:
+        return 1
+    if nb_buckets == 0:
+        return 0
+    return sum(get_nb_ways_split_number_in_buckets(n-i, nb_buckets-1) for i in range(n+1))
 
 def get_nb_arrangements(chars, groups):
     if not groups:
@@ -77,23 +91,83 @@ def get_nb_arrangements(chars, groups):
         cand = generate_smallest_combination(groups)
         return 1 if suffix_matches(cand, chars) else 0
 
-    # nb_full = sum(c == FULL for c in chars)
-    # nb_unknown = sum(c == UNKNOWN for c in chars)
-    # nb_empty = sum(c == EMPTY for c in chars)
-    # if nb_full == sum_groups:
-    #     cand = chars.replace(UNKNOWN, EMPTY)
-    #     return get_nb_arrangements(cand, groups)
-    # if nb_full > sum_groups:
-    #     return 0
-    # if nb_full + nb_unknown < sum_groups:
-    #     return 0
-    # if nb_full + nb_unknown == sum_groups:
-    #     cand = chars.replace(UNKNOWN, FULL)
-    #     return get_nb_arrangements(cand, groups)
+    nb_full = sum(c == FULL for c in chars)
+    nb_unknown = sum(c == UNKNOWN for c in chars)
+    nb_empty = sum(c == EMPTY for c in chars)
+    if nb_full == sum_groups:
+        cand = chars.replace(UNKNOWN, EMPTY)
+        return get_nb_arrangements(cand, groups)
+    if nb_full > sum_groups:
+        return 0
+    if nb_full + nb_unknown < sum_groups:
+        return 0
+    if nb_full + nb_unknown == sum_groups:
+        cand = chars.replace(UNKNOWN, FULL)
+        return get_nb_arrangements(cand, groups)
+
+    if nb_full + nb_empty == 0:
+        return get_nb_ways_split_number_in_buckets(freedom, len(groups) + 1)
+
+    # Divide and conquer on non-full indices
+    nonfull_indices = [i for i, c in enumerate(chars) if c != FULL]
+    if nonfull_indices and 0:
+        mid_idx_idx = len(nonfull_indices) // 2
+        assert 0 <= mid_idx_idx < len(nonfull_indices)
+        mid_idx = nonfull_indices[mid_idx_idx]
+        left, right = chars[:mid_idx], chars[mid_idx+1:]
+        assert len(left) + 1 + len(right) == len(chars)
+        mid_char = chars[mid_idx]
+        if mid_char == UNKNOWN:
+            ret = get_nb_arrangements("".join(c if i != mid_idx else FULL for i, c in enumerate(chars)), groups)
+        else:
+            assert mid_char == EMPTY
+            ret = 0
+        for i in range(len(groups)+1):
+            group_left, group_right = groups[:i], groups[i:]
+            ret_left = get_nb_arrangements(left, group_left)
+            if ret_left:
+                ret_right = get_nb_arrangements(right, group_right)
+                ret += ret_left * ret_right
+        return ret
+
+
+    # Divide and conquer on empty indices
+    empty_indices = [i for i, c in enumerate(chars) if c == EMPTY]
+    if empty_indices and 1:
+        mid_idx_idx = len(empty_indices) // 2
+        assert 0 <= mid_idx_idx < len(empty_indices)
+        mid_idx = empty_indices[mid_idx_idx]
+        left, right = chars[:mid_idx], chars[mid_idx+1:]
+        assert chars[mid_idx] == EMPTY
+        assert len(left) + 1 + len(right) == len(chars)
+        ret = 0
+        for i in range(len(groups)+1):
+            group_left, group_right = groups[:i], groups[i:]
+            ret_left = get_nb_arrangements(left, group_left)
+            if ret_left:
+                ret_right = get_nb_arrangements(right, group_right)
+                ret += ret_left * ret_right
+        return ret
+
+    # Divide and conquer on free indices
+    free_indices = [i for i, c in enumerate(chars) if c == UNKNOWN]
+    if free_indices and 1:
+        mid_idx_idx = len(free_indices) // 2
+        assert 0 <= mid_idx_idx < len(free_indices)
+        mid_idx = free_indices[mid_idx_idx]
+        left, right = chars[:mid_idx], chars[mid_idx+1:]
+        assert chars[mid_idx] == UNKNOWN
+        assert len(left) + 1 + len(right) == len(chars)
+        ret = get_nb_arrangements("".join(c if i != mid_idx else FULL for i, c in enumerate(chars)), groups)
+        for i in range(len(groups)+1):
+            group_left, group_right = groups[:i], groups[i:]
+            ret_left = get_nb_arrangements(left, group_left)
+            if ret_left:
+                ret_right = get_nb_arrangements(right, group_right)
+                ret += ret_left * ret_right
+        return ret
+
     ret = get_nb_arrangements(FULL + other_c, groups) + get_nb_arrangements(EMPTY + other_c, groups)
-    # if nb_full + nb_empty == 0:
-    #     # print(chars, groups, freedom, ret)
-    #     pass # There is an optimisation to be found here
     return ret
 
 
@@ -111,6 +185,14 @@ def run_tests():
     assert generate_smallest_combination([]) == ""
     assert generate_smallest_combination([2]) == "##"
     assert generate_smallest_combination([2, 3, 4]) == "##.###.####"
+    assert get_nb_ways_split_number_in_buckets(1, 2) == 2
+    assert get_nb_ways_split_number_in_buckets(2, 2) == 3
+    assert get_nb_ways_split_number_in_buckets(7, 2) == 8
+    assert get_nb_ways_split_number_in_buckets(1, 3) == 3
+    assert get_nb_ways_split_number_in_buckets(3, 3) == 10
+    assert get_nb_ways_split_number_in_buckets(1, 5) == 5
+    assert get_nb_ways_split_number_in_buckets(4, 5) == 70
+    assert get_nb_ways_split_number_in_buckets(30, 11) == 847660528
     records = get_records_from_lines(
         """???.### 1,1,3
 .??..??...?##. 1,1,3
@@ -122,12 +204,10 @@ def run_tests():
     assert get_sum_nb_arrangement(records) == 21
     assert get_sum_nb_arrangement(unfold_records(records)) == 525152
 
-
 def get_solutions():
     records = get_records_from_file()
     print(get_sum_nb_arrangement(records) == 6827)
-    # print(get_sum_nb_arrangement(unfold_records(records)))
-
+    print(get_sum_nb_arrangement(unfold_records(records)) == 1537505634471)
 
 if __name__ == "__main__":
     begin = datetime.datetime.now()
