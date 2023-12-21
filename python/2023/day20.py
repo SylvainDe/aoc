543e9c8c8055a8414c5a3140e5715b13085ad4d1
SylvainDe <sylvain.desodt+github@gmail.com>
2023-12-21 13:14:40 +0100

    Python - 2023 - Day 20 - Part 1

--- python/2023/day20.py
+++ python/2023/day20.py
@@ -43,39 +43,43 @@ def get_initial_state(modules):
             conjunctions[name] = { m: LOW for m in inputs[name] }
     return flipflops, conjunctions
 
-def process_event(modules, event, nb=1):
+
+def process_pulse(modules, input_event, nb=1):
     nb_low, nb_high = 0, 0
     flipflops, conjunctions = get_initial_state(modules)
-    d = collections.deque([event])
-    while d:
-        event = d.popleft()
-        src, module_name, level = event
-        assert level in (LOW, HIGH)
-        if level == LOW:
-            nb_low += 1
-        else:
-            nb_high += 1
-        print(src, "-" + ("low" if level == LOW else "high") + "->", module_name)
-        prefix, dests = modules[module_name]
-        level_to_send = None
-        if prefix == "%":
+    for _ in range(nb):
+        d = collections.deque([input_event])
+        while d:
+            event = d.popleft()
+            src, module_name, level = event
+            assert level in (LOW, HIGH)
             if level == LOW:
-                previous_state = flipflops[module_name]
-                assert previous_state in (ON, OFF)
-                level_to_send = HIGH if previous_state == OFF else LOW
-                flipflops[module_name] = OFF if previous_state == ON else ON
-        elif prefix == "&":
-            conj = conjunctions[module_name]
-            conj[src] = level
-            level_to_send = LOW if all(l == HIGH for l in conj.values()) else HIGH
-        else:
-            assert prefix is None
-            level_to_send = level
-        if level_to_send is not None:
-            for dst in dests:
-                new_event = (module_name, dst, level_to_send)
-                d.append((new_event))
-    return nb_low * nb_high
+                nb_low += 1
+            else:
+                nb_high += 1
+            # print(src, "-" + ("low" if level == LOW else "high") + "->", module_name)
+            prefix, dests = modules.get(module_name, (None, []))
+            level_to_send = None
+            if prefix == "%":
+                if level == LOW:
+                    previous_state = flipflops[module_name]
+                    assert previous_state in (ON, OFF)
+                    level_to_send = HIGH if previous_state == OFF else LOW
+                    flipflops[module_name] = OFF if previous_state == ON else ON
+            elif prefix == "&":
+                conj = conjunctions[module_name]
+                conj[src] = level
+                level_to_send = LOW if all(l == HIGH for l in conj.values()) else HIGH
+            else:
+                assert prefix is None
+                level_to_send = level
+            if level_to_send is not None:
+                for dst in dests:
+                    new_event = (module_name, dst, level_to_send)
+                    d.append((new_event))
+    ret = nb_low * nb_high
+    # print(nb, nb_low, nb_high, ret)
+    return ret
 
 def run_tests():
     modules = get_modules_from_lines(
@@ -85,8 +89,8 @@ def run_tests():
 %c -> inv
 &inv -> a"""
     )
-    assert process_event(modules, BUTTON_PRESS) == 8 * 4
-    # assert process_event(modules, BUTTON_PRESS, 2) == 2 * 8 * 4
+    for nb_rep in (1, 2, 1000):
+        assert process_pulse(modules, BUTTON_PRESS, nb_rep) == (nb_rep * 8) * (nb_rep * 4)
     modules = get_modules_from_lines(
         """broadcaster -> a
 %a -> inv, con
@@ -94,13 +98,16 @@ def run_tests():
 %b -> con
 &con -> output"""
     )
-    process_event(modules, BUTTON_PRESS)
-    #process_event(modules, BUTTON_PRESS, 2)
+    assert process_pulse(modules, BUTTON_PRESS, 1) == 4 * 4
+    assert process_pulse(modules, BUTTON_PRESS, 2) == 8 * 6
+    assert process_pulse(modules, BUTTON_PRESS, 3) == 13 * 9
+    assert process_pulse(modules, BUTTON_PRESS, 1000) == 4250 * 2750
 
 
 
 def get_solutions():
     modules = get_modules_from_file()
+    print(process_pulse(modules, BUTTON_PRESS, 1000) == 818723272)
 
 
 if __name__ == "__main__":
