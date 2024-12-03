fcd098c90e66ae5f0ea3ed3c70cbdf9b59086973
SylvainDe <sylvain.desodt+github@gmail.com>
2024-12-03 06:27:10 +0100

    Python - 2024 - Day 3 - Part 1 and 2

--- python/2024/day3.py
+++ python/2024/day3.py
@@ -1,34 +1,54 @@
 # vi: set shiftwidth=4 tabstop=4 expandtab:
 import datetime
 import os
-
+import re
 
 top_dir = os.path.dirname(os.path.abspath(__file__)) + "/../../"
 
 
-def get_xxx_from_line(string):
-    return string
+def get_strings_from_lines(string):
+    return list(string.splitlines())
 
 
-def get_xxxs_from_lines(string):
-    return [get_xxx_from_line(l) for l in string.splitlines()]
-
-
-def get_xxxs_from_file(file_path=top_dir + "resources/year2024_day3_input.txt"):
+def get_string_from_file(file_path=top_dir + "resources/year2024_day3_input.txt"):
     with open(file_path) as f:
-        return get_xxxs_from_lines(f.read())
+        return get_strings_from_lines(f.read())
+
+
+def get_mult_sum(strings):
+    s = " ".join(strings)
+    return sum(int(a) * int(b) for a, b in re.findall("mul\((\d+),(\d+)\)", s))
+
+
+def get_enabled_mult_sum(strings):
+    s = " ".join(strings)
+    sum_ = 0
+    do = True
+    for instr, a, b in re.findall("(mul\((\d+),(\d+)\)|don't\(\)|do\(\))", s):
+        if instr == "don't()":
+            do = False
+        elif instr == "do()":
+            do = True
+        elif do:
+            sum_ += int(a) * int(b)
+    return sum_
 
 
 def run_tests():
-    xxxs = get_xxxs_from_lines(
-        """abc
-def
-ghi"""
+    strings = get_strings_from_lines(
+        """xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"""
     )
+    assert get_mult_sum(strings) == 161
+    strings = get_strings_from_lines(
+        """xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"""
+    )
+    assert get_enabled_mult_sum(strings) == 48
 
 
 def get_solutions():
-    xxxs = get_xxxs_from_file()
+    string = get_string_from_file()
+    print(get_mult_sum(string) == 184576302)
+    print(get_enabled_mult_sum(string) == 118173507)
 
 
 if __name__ == "__main__":
