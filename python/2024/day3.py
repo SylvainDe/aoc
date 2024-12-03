5aad036cedf955b09271f5d44e8e5a4c0c96c512
SylvainDe <sylvain.desodt+github@gmail.com>
2024-12-03 06:37:25 +0100

    Python - 2024 - Day 3 - cleanup

--- python/2024/day3.py
+++ python/2024/day3.py
@@ -6,49 +6,45 @@ import re
 top_dir = os.path.dirname(os.path.abspath(__file__)) + "/../../"
 
 
-def get_strings_from_lines(string):
-    return list(string.splitlines())
+def get_string_from_lines(string):
+    return " ".join(string.splitlines())
 
 
 def get_string_from_file(file_path=top_dir + "resources/year2024_day3_input.txt"):
     with open(file_path) as f:
-        return get_strings_from_lines(f.read())
+        return get_string_from_lines(f.read())
 
 
-def get_mult_sum(strings):
-    s = " ".join(strings)
-    return sum(int(a) * int(b) for a, b in re.findall("mul\((\d+),(\d+)\)", s))
-
-
-def get_enabled_mult_sum(strings):
-    s = " ".join(strings)
-    sum_ = 0
+def get_mults(s, always_enabled):
     do = True
     for instr, a, b in re.findall("(mul\((\d+),(\d+)\)|don't\(\)|do\(\))", s):
         if instr == "don't()":
             do = False
         elif instr == "do()":
             do = True
-        elif do:
-            sum_ += int(a) * int(b)
-    return sum_
+        elif always_enabled or do:
+            yield int(a) * int(b)
+
+
+def get_enabled_mult_sum(s, always_enabled):
+    return sum(get_mults(s, always_enabled))
 
 
 def run_tests():
-    strings = get_strings_from_lines(
+    string = get_string_from_lines(
         """xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"""
     )
-    assert get_mult_sum(strings) == 161
-    strings = get_strings_from_lines(
+    assert get_enabled_mult_sum(string, True) == 161
+    string = get_string_from_lines(
         """xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"""
     )
-    assert get_enabled_mult_sum(strings) == 48
+    assert get_enabled_mult_sum(string, False) == 48
 
 
 def get_solutions():
     string = get_string_from_file()
-    print(get_mult_sum(string) == 184576302)
-    print(get_enabled_mult_sum(string) == 118173507)
+    print(get_enabled_mult_sum(string, True) == 184576302)
+    print(get_enabled_mult_sum(string, False) == 118173507)
 
 
 if __name__ == "__main__":
