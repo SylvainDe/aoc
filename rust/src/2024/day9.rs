4c9949ef7ac55172d40607dcb7cb650cb91ae490
SylvainDe <sylvain.desodt+github@gmail.com>
2024-12-09 07:10:42 +0100

    Year 2024 - Day 9 - Getting started

---
+++ rust/src/2024/day9.rs
@@ -0,0 +1,54 @@
+// vi: set shiftwidth=4 tabstop=4 expandtab:
+use common::input::check_answer;
+use common::input::get_answers;
+use common::input::get_first_line_from_file;
+use std::time::Instant;
+
+const INPUT_FILEPATH: &str = "../resources/year2024_day9_input.txt";
+const ANSWERS_FILEPATH: &str = "../resources/year2024_day9_answer.txt";
+
+type Int = u32;
+type InputContent = String;
+
+fn get_input_from_str(string: &str) -> InputContent {
+    string.to_owned()
+}
+
+#[expect(clippy::missing_const_for_fn, reason = "Not implemented yet")]
+fn part1(_arg: &InputContent) -> Int {
+    0
+}
+
+#[expect(clippy::missing_const_for_fn, reason = "Not implemented yet")]
+fn part2(_arg: &InputContent) -> Int {
+    0
+}
+
+fn main() {
+    let before = Instant::now();
+    let data = get_input_from_str(&get_first_line_from_file(INPUT_FILEPATH));
+    let (ans, ans2) = get_answers(ANSWERS_FILEPATH);
+    let solved = false;
+    let res = part1(&data);
+    check_answer(&res.to_string(), ans, solved);
+    let res2 = part2(&data);
+    check_answer(&res2.to_string(), ans2, solved);
+    println!("Elapsed time: {:.2?}", before.elapsed());
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const EXAMPLE: &str = "";
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(part1(&get_input_from_str(EXAMPLE)), 0);
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2(&get_input_from_str(EXAMPLE)), 0);
+    }
+}
