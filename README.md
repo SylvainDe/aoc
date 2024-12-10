6d6932ec32248ead66ae80a0d44906572dc84c68
SylvainDe <sylvain.desodt+github@gmail.com>
2024-12-10 09:07:21 +0100

    Python - 2024 - Day 10 - Part 1 and 2

--- README.md
+++ README.md
@@ -34,8 +34,8 @@ Solutions are written in Python and/or Rust.
 |Day 7: Bridge Repair|[Problem](https://adventofcode.com/2024/day/7) [Input](https://adventofcode.com/2024/day/7/input)|[puzzle.html](resources/year2024_day7_puzzle.html) [input.txt](resources/year2024_day7_input.txt)|**|[day7.py](python/2024/day7.py)|[-](rust/src/2024/day7.rs)|00:07:10|00:15:58|
 |Day 8: Resonant Collinearity|[Problem](https://adventofcode.com/2024/day/8) [Input](https://adventofcode.com/2024/day/8/input)|[puzzle.html](resources/year2024_day8_puzzle.html) [input.txt](resources/year2024_day8_input.txt)|**|[day8.py](python/2024/day8.py)|[-](rust/src/2024/day8.rs)|01:50:11|01:56:23|
 |Day 9: Disk Fragmenter|[Problem](https://adventofcode.com/2024/day/9) [Input](https://adventofcode.com/2024/day/9/input)|[puzzle.html](resources/year2024_day9_puzzle.html) [input.txt](resources/year2024_day9_input.txt)|**|[day9.py](python/2024/day9.py)|[-](rust/src/2024/day9.rs)|03:00:41|03:34:30|
-|Day 10: Hoof It|[Problem](https://adventofcode.com/2024/day/10) [Input](https://adventofcode.com/2024/day/10/input)|[puzzle.html](resources/year2024_day10_puzzle.html) [input.txt](resources/year2024_day10_input.txt)||[-](python/2024/day10.py)|[-](rust/src/2024/day10.rs)|-|-|
-|[2024](https://adventofcode.com/2024)|[Stats](https://adventofcode.com/2024/leaderboard/self)|[stats.txt](misc/leaderboard_self_2024.html)|18|-|-|-|-|
+|Day 10: Hoof It|[Problem](https://adventofcode.com/2024/day/10) [Input](https://adventofcode.com/2024/day/10/input)|[puzzle.html](resources/year2024_day10_puzzle.html) [input.txt](resources/year2024_day10_input.txt)|**|[day10.py](python/2024/day10.py)|[-](rust/src/2024/day10.rs)|02:55:13|02:55:32|
+|[2024](https://adventofcode.com/2024)|[Stats](https://adventofcode.com/2024/leaderboard/self)|[stats.txt](misc/leaderboard_self_2024.html)|20|-|-|-|-|
 
 ## [2023](https://adventofcode.com/2023)
 |Day|URLs|Puzzle & Input|Stars|Python|Rust|Time part 1|Time part 2|
@@ -319,7 +319,7 @@ Solutions are written in Python and/or Rust.
 |7|**|**|**|**|*|**|**|**|**|**|19: *******************|
 |8|**|**|**|**|**|**|**|**|**|**|20: ********************|
 |9|**|**|**||**|**|**|**|**|**|18: ******************|
-|10|**|**|**|**|**|**|**|**|**||18: ******************|
+|10|**|**|**|**|**|**|**|**|**|**|20: ********************|
 |11||**|**|**||**|**|**|**||14: **************|
 |12|**|**|**||**|**|**|**|**||16: ****************|
 |13||**|**|||**|**|**|**||12: ************|
@@ -335,4 +335,4 @@ Solutions are written in Python and/or Rust.
 |23||**||||**|*|**||| 7: *******|
 |24||**|||**|**||**|*|| 9: *********|
 |25|*|*||*||**|*|**||| 8: ********|
-|Total|30|48|28|23|29|50|46|50|39|18|361|
+|Total|30|48|28|23|29|50|46|50|39|20|363|
--- python/2024/day10.py
+++ python/2024/day10.py
@@ -1,34 +1,97 @@
 # vi: set shiftwidth=4 tabstop=4 expandtab:
 import datetime
 import os
+import collections
 
 
 top_dir = os.path.dirname(os.path.abspath(__file__)) + "/../../"
 
 
-def get_xxx_from_line(string):
-    return string
+def get_grid_from_lines(string):
+    grid = dict()
+    for i, l in enumerate(string.splitlines()):
+        for j, v in enumerate(l):
+            grid[(i, j)] = int(v) if v != "." else -4
+    return grid
 
 
-def get_xxxs_from_lines(string):
-    return [get_xxx_from_line(l) for l in string.splitlines()]
-
-
-def get_xxxs_from_file(file_path=top_dir + "resources/year2024_day10_input.txt"):
+def get_grid_from_file(file_path=top_dir + "resources/year2024_day10_input.txt"):
     with open(file_path) as f:
-        return get_xxxs_from_lines(f.read())
+        return get_grid_from_lines(f.read())
+
+
+def get_neighbours(pos):
+    x, y = pos
+    yield x, y + 1
+    yield x, y - 1
+    yield x + 1, y
+    yield x - 1, y
+
+
+def get_neighbours_with_val(pos, grid, val):
+    for neig in get_neighbours(pos):
+        if grid.get(neig, -1) == val:
+            yield neig
+
+
+def get_trailhead_score_sum(grid):
+    reachable = set((p, p) for p, v in grid.items() if v == 0)
+    for v in range(9):
+        assert all(grid[begin] == 0 for begin, _ in reachable)
+        assert all(grid[pos] == v for _, pos in reachable)
+        new_reachable = set()
+        for begin, pos in reachable:
+            new_reachable.update(
+                (begin, neig) for neig in get_neighbours_with_val(pos, grid, v + 1)
+            )
+        reachable = new_reachable
+        assert all(grid[pos] == v + 1 for _, pos in reachable)
+    return len(reachable)
+
+
+def get_trailhead_score_sum2(grid):
+    reachable = {p: 1 for p, v in grid.items() if v == 0}
+    for v in range(9):
+        assert all(grid[pos] == v for pos in reachable)
+        new_reachable = collections.defaultdict(int)
+        for pos, nb in reachable.items():
+            for neig in get_neighbours_with_val(pos, grid, v + 1):
+                new_reachable[neig] += nb
+        reachable = new_reachable
+        assert all(grid[pos] == v + 1 for pos in reachable)
+    return sum(reachable.values())
 
 
 def run_tests():
-    xxxs = get_xxxs_from_lines(
-        """abc
-def
-ghi"""
+    grid = get_grid_from_lines(
+        """10..9..
+2...8..
+3...7..
+4567654
+...8..3
+...9..2
+.....01"""
     )
+    assert get_trailhead_score_sum(grid) == 3
+
+    grid = get_grid_from_lines(
+        """89010123
+78121874
+87430965
+96549874
+45678903
+32019012
+01329801
+10456732"""
+    )
+    assert get_trailhead_score_sum(grid) == 36
+    assert get_trailhead_score_sum2(grid) == 81
 
 
 def get_solutions():
-    xxxs = get_xxxs_from_file()
+    grid = get_grid_from_file()
+    print(get_trailhead_score_sum(grid) == 566)
+    print(get_trailhead_score_sum2(grid) == 1324)
 
 
 if __name__ == "__main__":
