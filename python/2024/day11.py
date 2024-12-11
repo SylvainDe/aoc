ebcaa31d60205e39b2ee97482aaadc5434760471
SylvainDe <sylvain.desodt+github@gmail.com>
2024-12-11 08:41:30 +0100

    Python - 2024 - Day 11 - Part 2

--- python/2024/day11.py
+++ python/2024/day11.py
@@ -1,7 +1,8 @@
 # vi: set shiftwidth=4 tabstop=4 expandtab:
 import datetime
 import os
-
+from functools import lru_cache
+import collections
 
 top_dir = os.path.dirname(os.path.abspath(__file__)) + "/../../"
 
@@ -30,29 +31,61 @@ def get_next_steps(s):
         yield 2024 * s
 
 
-def blink(stones):
-    return [s2 for s in stones for s2 in get_next_steps(s)]
-
-
 def blink_n(stones, n):
     for i in range(n):
-        print(i)
-        stones = blink(stones)
-    return len(stones)
+        stones = [s2 for s in stones for s2 in get_next_steps(s)]
+    return stones
+
+
+def blink_n_with_counter(stones, n):
+    stones = collections.Counter(stones)
+    for i in range(n):
+        c = collections.Counter()
+        for s, nb in stones.items():
+            for s2 in get_next_steps(s):
+                c[s2] += nb
+        stones = c
+    return sum(stones.values())
 
 
 def run_tests():
     stones = get_stones_from_line("0 1 10 99 999")
-    assert blink(stones) == [1, 2024, 1, 0, 9, 9, 2021976]
+    assert blink_n(stones, 1) == [1, 2024, 1, 0, 9, 9, 2021976]
     stones = get_stones_from_line("125 17")
-    assert blink_n(stones, 6) == 22
-    assert blink_n(stones, 25) == 55312
+    assert blink_n(stones, 6) == [
+        2097446912,
+        14168,
+        4048,
+        2,
+        0,
+        2,
+        4,
+        40,
+        48,
+        2024,
+        40,
+        48,
+        80,
+        96,
+        2,
+        8,
+        6,
+        7,
+        6,
+        0,
+        3,
+        2,
+    ]
+    assert len(blink_n(stones, 6)) == 22
+    assert len(blink_n(stones, 25)) == 55312
+    assert blink_n_with_counter(stones, 6) == 22
+    assert blink_n_with_counter(stones, 25) == 55312
 
 
 def get_solutions():
     stones = get_stones_from_file()
-    print(blink_n(stones, 25))
-    # print(blink_n(stones, 75))
+    print(len(blink_n(stones, 25)) == 182081)
+    print(blink_n_with_counter(stones, 75) == 216318908621637)
 
 
 if __name__ == "__main__":
