8e1c68dad09ad8f5344254334b74dd8cfaba111d
SylvainDe <sylvain.desodt+github@gmail.com>
2024-12-15 20:05:07 +0100

    Year 2024 - Day 15 - Part 1 - cleanup

--- python/2024/day15.py
+++ python/2024/day15.py
@@ -7,13 +7,14 @@ top_dir = os.path.dirname(os.path.abspath(__file__)) + "/../../"
 
 directions = {
     "^": (-1, 0),
-    "v": (1, 0), 
+    "v": (1, 0),
     ">": (0, 1),
     "<": (0, -1),
 }
 
-WALL = '#'
-BOX = 'O'
+WALL = "#"
+BOX = "O"
+
 
 def get_grid_content(strings):
     grid = dict()
@@ -21,9 +22,9 @@ def get_grid_content(strings):
     for i, l in enumerate(strings):
         for j, v in enumerate(l):
             pos = (i, j)
-            if v == '.':
+            if v == ".":
                 continue
-            elif v == '@':
+            elif v == "@":
                 assert robot is None
                 robot = pos
                 continue
@@ -31,11 +32,13 @@ def get_grid_content(strings):
             grid[pos] = v
     return grid, robot
 
+
 def get_instructions(string):
     return [directions[c] for c in string if c in directions]
 
+
 def get_inputs_from_lines(string):
-    sep = '\n\n'
+    sep = "\n\n"
     beg, mid, end = string.partition(sep)
     assert mid == sep
     grid, robot = get_grid_content(beg.splitlines())
@@ -46,17 +49,16 @@ def get_inputs_from_file(file_path=top_dir + "resources/year2024_day15_input.txt
     with open(file_path) as f:
         return get_inputs_from_lines(f.read())
 
+
 def gps(pos):
     x, y = pos
-    ret = 100 * x + y
-    # print(pos, ret)
-    return ret
+    return 100 * x + y
+
 
 def follow_instructions(grid, robot, instr):
     i, j = robot
     for di, dj in instr:
         do_follow_instructions = True
-        boxes = []
         for step in itertools.count(start=1):
             pos = i + step * di, j + step * dj
             content = grid.get(pos, None)
@@ -66,7 +68,6 @@ def follow_instructions(grid, robot, instr):
                 do_follow_instructions = False
             else:
                 assert content == BOX
-                boxes.append(pos)
         if do_follow_instructions:
             free_pos = i + step * di, j + step * dj
             assert free_pos not in grid
@@ -74,13 +75,8 @@ def follow_instructions(grid, robot, instr):
             if (i, j) in grid:
                 grid[free_pos] = grid[(i, j)]
                 grid.pop((i, j))
-    # for x in range(0, 1 + max(x for x, y in grid)):
-    #     print("".join(grid.get((x, y), " ") for y in range(0, 1 + max(y for x, y in grid))))
-    # print()
-    ret = sum(gps(p) for p, v in grid.items() if v == BOX)
-    # print(ret)
-    return ret 
-        
+    return sum(gps(p) for p, v in grid.items() if v == BOX)
+
 
 def run_tests():
     grid, robot, instr = get_inputs_from_lines(
@@ -105,9 +101,11 @@ vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v
 >^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^
 <><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>
 ^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>
-v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^""")
+v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^"""
+    )
     assert follow_instructions(grid, robot, instr) == 10092
-    grid, robot, instr = get_inputs_from_lines("""########
+    grid, robot, instr = get_inputs_from_lines(
+        """########
 #..O.O.#
 ##@.O..#
 #...O..#
@@ -116,13 +114,14 @@ v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^""")
 #......#
 ########
 
-<^^>>>vv<v>>v<<""")
+<^^>>>vv<v>>v<<"""
+    )
     assert follow_instructions(grid, robot, instr) == 2028
 
 
 def get_solutions():
     grid, robot, instr = get_inputs_from_file()
-    print(follow_instructions(grid, robot, instr))
+    print(follow_instructions(grid, robot, instr) == 1559280)
 
 
 if __name__ == "__main__":
