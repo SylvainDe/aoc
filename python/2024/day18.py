47f732080038acb32768f5ec11bff4d0c4140a58
SylvainDe <sylvain.desodt+github@gmail.com>
2024-12-19 19:04:26 +0100

    Python - 2024 - Day 18 - Parsing

--- python/2024/day18.py
+++ python/2024/day18.py
@@ -6,29 +6,65 @@ import os
 top_dir = os.path.dirname(os.path.abspath(__file__)) + "/../../"
 
 
-def get_xxx_from_line(string):
-    return string
+def get_block_from_line(string):
+    return tuple(int(v) for v in string.split(","))
 
 
-def get_xxxs_from_lines(string):
-    return [get_xxx_from_line(l) for l in string.splitlines()]
+def get_blocks_from_lines(string):
+    return [get_block_from_line(l) for l in string.splitlines()]
 
 
-def get_xxxs_from_file(file_path=top_dir + "resources/year2024_day18_input.txt"):
+def get_blocks_from_file(file_path=top_dir + "resources/year2024_day18_input.txt"):
     with open(file_path) as f:
-        return get_xxxs_from_lines(f.read())
+        return get_blocks_from_lines(f.read())
+
+
+def display(blocks):
+    xs = set(x for x, _ in blocks)
+    ys = set(y for _, y in blocks)
+    for y in range(min(ys), max(ys) + 1):
+        print(
+            "".join(
+                "#" if (x, y) in blocks else " " for x in range(min(xs), max(xs) + 1)
+            )
+        )
 
 
 def run_tests():
-    xxxs = get_xxxs_from_lines(
-        """abc
-def
-ghi"""
+    blocks = get_blocks_from_lines(
+        """5,4
+4,2
+4,5
+3,0
+2,1
+6,3
+2,4
+1,5
+0,6
+3,3
+2,6
+5,1
+1,2
+5,5
+2,5
+6,5
+1,4
+0,4
+6,4
+1,1
+6,1
+1,0
+0,5
+1,6
+2,0"""
     )
+    blocks12 = set(blocks[:12])
+    display(blocks12)
 
 
 def get_solutions():
-    xxxs = get_xxxs_from_file()
+    blocks = get_blocks_from_file()
+    blocks1024 = set(blocks[:1024])
 
 
 if __name__ == "__main__":
