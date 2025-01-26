98af4aed86e08562827e79a3c6a61af55ce29140
SylvainDe <sylvain.desodt+github@gmail.com>
2025-01-26 11:28:58 +0100

    Python - 2017 - Day 15 - Parse input

--- python/2017/day15.py
+++ python/2017/day15.py
@@ -1,34 +1,47 @@
 # vi: set shiftwidth=4 tabstop=4 expandtab:
 import datetime
 import os
+import re
 
 
 top_dir = os.path.dirname(os.path.abspath(__file__)) + "/../../"
 
 
-def get_xxx_from_line(string):
-    return string
+# Example: "Generator A starts with 42"
+gen_re = re.compile(r"Generator (?P<name>.*) starts with (?P<value>\d+)")
 
 
-def get_xxxs_from_lines(string):
-    return [get_xxx_from_line(l) for l in string.splitlines()]
+def generator(name, n):
+    factor = 16807 if name == "A" else 48271
+    while True:
+        n *= factor
+        n %= 2147483647
+        yield n
 
 
-def get_xxxs_from_file(file_path=top_dir + "resources/year2017_day15_input.txt"):
+def get_generator_from_line(string):
+    d = gen_re.match(string).groupdict()
+    return d["name"], int(d["value"])
+
+
+def get_generators_from_lines(string):
+    return [get_generator_from_line(l) for l in string.splitlines()]
+
+
+def get_generators_from_file(file_path=top_dir + "resources/year2017_day15_input.txt"):
     with open(file_path) as f:
-        return get_xxxs_from_lines(f.read())
+        return get_generators_from_lines(f.read())
 
 
 def run_tests():
-    xxxs = get_xxxs_from_lines(
-        """abc
-def
-ghi"""
+    generator_values = get_generators_from_lines(
+        """Generator A starts with 65
+Generator B starts with 8921"""
     )
 
 
 def get_solutions():
-    xxxs = get_xxxs_from_file()
+    generator_values = get_generators_from_file()
 
 
 if __name__ == "__main__":
