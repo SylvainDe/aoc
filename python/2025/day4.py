d9daa6d6bf5679d481fd4ea1e8a084f5d8654d80
SylvainDe <sylvain.desodt+github@gmail.com>
2025-12-04 10:19:03 +0100

    Python - 2025 - Day 4 - Part 1 & 2

--- python/2025/day4.py
+++ python/2025/day4.py
@@ -1,34 +1,75 @@
 # vi: set shiftwidth=4 tabstop=4 expandtab:
 import datetime
 import os
-
+import itertools
 
 top_dir = os.path.dirname(os.path.abspath(__file__)) + "/../../"
 
 
-def get_xxx_from_line(string):
-    return string
+def get_rolls_from_lines(string):
+    return {
+        (i, j)
+        for i, l in enumerate(string.splitlines())
+        for j, c in enumerate(l)
+        if c == "@"
+    }
 
 
-def get_xxxs_from_lines(string):
-    return [get_xxx_from_line(l) for l in string.splitlines()]
-
-
-def get_xxxs_from_file(file_path=top_dir + "resources/year2025_day4_input.txt"):
+def get_rolls_from_file(file_path=top_dir + "resources/year2025_day4_input.txt"):
     with open(file_path) as f:
-        return get_xxxs_from_lines(f.read())
+        return get_rolls_from_lines(f.read())
+
+
+def neighbours(point):
+    x, y = point
+    for dx, dy in itertools.product([-1, 0, 1], repeat=2):
+        if (dx, dy) != (0, 0):
+            yield x + dx, y + dy
+
+
+def is_available(roll, rolls):
+    return sum(r2 in rolls for r2 in neighbours(roll)) < 4
+
+
+def get_available_rolls(rolls):
+    return (r for r in rolls if is_available(r, rolls))
+
+
+def get_nb_available_rolls(rolls):
+    return sum(1 for r in get_available_rolls(rolls))
+
+
+def get_available_rolls_with_removal(rolls):
+    available = set()
+    while True:
+        to_remove = set(get_available_rolls(rolls))
+        if not to_remove:
+            return available
+        rolls = rolls - to_remove
+        available |= to_remove
 
 
 def run_tests():
-    xxxs = get_xxxs_from_lines(
-        """abc
-def
-ghi"""
+    rolls = get_rolls_from_lines(
+        """..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@."""
     )
+    assert get_nb_available_rolls(rolls) == 13
+    assert len(get_available_rolls_with_removal(rolls)) == 43
 
 
 def get_solutions():
-    xxxs = get_xxxs_from_file()
+    rolls = get_rolls_from_file()
+    print(get_nb_available_rolls(rolls) == 1523)
+    print(len(get_available_rolls_with_removal(rolls)) == 9290)
 
 
 if __name__ == "__main__":
