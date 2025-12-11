831a1a15744b376bd60f2a35f7729fb5afaa3ef2
SylvainDe <sylvain.desodt+github@gmail.com>
2025-12-11 13:26:28 +0100

    Python - 2025 - Day 11 - Part 2 to be optimised

--- python/2025/day11.py
+++ python/2025/day11.py
@@ -2,7 +2,7 @@
 import datetime
 import os
 import collections
-
+import math
 
 top_dir = os.path.dirname(os.path.abspath(__file__)) + "/../../"
 
@@ -31,8 +31,18 @@ def get_paths(devices, beg="you", end="out"):
         if dev == end:
             ret += nb
         else:
-            for out in devices[dev]:
+            for out in devices.get(dev, ()):
                 paths[out] += nb
+    return ret
+
+
+def get_paths2(devices):
+    beg, end = "svr", "out"
+    steps = ["dac", "fft"]
+    ret = 0
+    for order in [steps, list(reversed(steps))]:
+        order = [beg] + order + [end]
+        ret += math.prod(get_paths(devices, s1, s2) for s1, s2 in zip(order, order[1:]))
     return ret
 
 
@@ -50,11 +60,27 @@ hhh: ccc fff iii
 iii: out"""
     )
     assert get_paths(devices) == 5
+    devices = get_devices_from_lines(
+        """svr: aaa bbb
+aaa: fft
+fft: ccc
+bbb: tty
+tty: ccc
+ccc: ddd eee
+ddd: hub
+hub: fff
+eee: dac
+dac: fff
+fff: ggg hhh
+ggg: out
+hhh: out""")
+    assert get_paths2(devices) == 2
 
 
 def get_solutions():
     devices = get_devices_from_file()
-    print(get_paths(devices))
+    print(get_paths(devices) == 749)
+# NEEDS OPTIMISATION   print(get_paths2(devices))
 
 
 if __name__ == "__main__":
