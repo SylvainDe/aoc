0c3b357655fb25453151ac352bffb44fbccb078f
SylvainDe <sylvain.desodt+github@gmail.com>
2025-12-11 10:01:35 +0100

    Python - 2025 - Day 11 - Part 1

--- python/2025/day11.py
+++ python/2025/day11.py
@@ -1,34 +1,60 @@
 # vi: set shiftwidth=4 tabstop=4 expandtab:
 import datetime
 import os
+import collections
 
 
 top_dir = os.path.dirname(os.path.abspath(__file__)) + "/../../"
 
 
-def get_xxx_from_line(string):
-    return string
+def get_device_from_line(string):
+    sep = ": "
+    name, mid, outputs = string.partition(sep)
+    assert mid == sep
+    return name, tuple(outputs.split(" "))
 
 
-def get_xxxs_from_lines(string):
-    return [get_xxx_from_line(l) for l in string.splitlines()]
+def get_devices_from_lines(string):
+    return dict(get_device_from_line(l) for l in string.splitlines())
 
 
-def get_xxxs_from_file(file_path=top_dir + "resources/year2025_day11_input.txt"):
+def get_devices_from_file(file_path=top_dir + "resources/year2025_day11_input.txt"):
     with open(file_path) as f:
-        return get_xxxs_from_lines(f.read())
+        return get_devices_from_lines(f.read())
+
+
+def get_paths(devices, beg="you", end="out"):
+    ret = 0
+    paths = collections.Counter([beg])
+    while paths:
+        dev, nb = paths.popitem()
+        if dev == end:
+            ret += nb
+        else:
+            for out in devices[dev]:
+                paths[out] += nb
+    return ret
 
 
 def run_tests():
-    xxxs = get_xxxs_from_lines(
-        """abc
-def
-ghi"""
+    devices = get_devices_from_lines(
+        """aaa: you hhh
+you: bbb ccc
+bbb: ddd eee
+ccc: ddd eee fff
+ddd: ggg
+eee: out
+fff: out
+ggg: out
+hhh: ccc fff iii
+iii: out"""
     )
+    assert get_paths(devices) == 5
 
 
 def get_solutions():
-    xxxs = get_xxxs_from_file()
+    devices = get_devices_from_file()
+    print(get_paths(devices))
 
 
 if __name__ == "__main__":
