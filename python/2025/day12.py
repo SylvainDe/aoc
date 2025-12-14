039ed9b41bb5bad2a11c319584c830b2c979bd72
SylvainDe <sylvain.desodt+github@gmail.com>
2025-12-14 15:23:32 +0100

    Python - 2025 - Day 12 - Parsing

--- python/2025/day12.py
+++ python/2025/day12.py
@@ -6,29 +6,76 @@ import os
 top_dir = os.path.dirname(os.path.abspath(__file__)) + "/../../"
 
 
-def get_xxx_from_line(string):
-    return string
+def get_shape_from_lines(string):
+    return {
+        (i, j)
+        for i, l in enumerate(string.splitlines()[1:])
+        for j, v in enumerate(l)
+        if v == "#"
+    }
+
+def get_region_from_line(string):
+    sep = ": "
+    beg, mid, end = string.partition(sep)
+    assert mid == sep
+    dims = tuple(int(i) for i in beg.split("x"))
+    qtys = tuple(int(i) for i in end.split(" "))
+    return dims, qtys
 
 
-def get_xxxs_from_lines(string):
-    return [get_xxx_from_line(l) for l in string.splitlines()]
+def get_input_from_lines(string):
+    lst = string.split("\n\n")
+    shapes = tuple(get_shape_from_lines(s) for s in lst[:-1])
+    regions = tuple(get_region_from_line(l) for l in lst[-1].splitlines())
+    return shapes, regions
 
 
-def get_xxxs_from_file(file_path=top_dir + "resources/year2025_day12_input.txt"):
+def get_input_from_file(file_path=top_dir + "resources/year2025_day12_input.txt"):
     with open(file_path) as f:
-        return get_xxxs_from_lines(f.read())
+        return get_input_from_lines(f.read())
 
 
 def run_tests():
-    xxxs = get_xxxs_from_lines(
-        """abc
-def
-ghi"""
+    input_ = get_input_from_lines(
+        """0:
+###
+##.
+##.
+
+1:
+###
+##.
+.##
+
+2:
+.##
+###
+##.
+
+3:
+##.
+###
+##.
+
+4:
+###
+#..
+###
+
+5:
+###
+.#.
+###
+
+4x4: 0 0 0 0 2 0
+12x5: 1 0 1 0 2 2
+12x5: 1 0 1 0 3 2"""
     )
+    print(input_)
 
 
 def get_solutions():
-    xxxs = get_xxxs_from_file()
+    input_ = get_input_from_file()
 
 
 if __name__ == "__main__":
